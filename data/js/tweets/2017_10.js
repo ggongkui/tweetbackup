2a4db99af116973cc40aeafba8fc12368502151a
ggongkui <32505785+ggongkui@users.noreply.github.com>
2017-10-10 09:15:31 +0900

    Added 1 tweets

--- data/js/tweets/2017_10.js
+++ data/js/tweets/2017_10.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2017_10 = 
 [
 	{
+		"created_at": "Sun Oct 08 13:59:45 +0000 2017",
+		"id": 917026733957587000,
+		"id_str": "917026733957586946",
+		"text": "RT @O_ZZ_U: 내가 신청서 쓸 때/자캐 짤 때 참고하는것들\nMBTI 테스트 https://t.co/IB9NuHsoLy\n에고그램 테스트 https://t.co/aXJ7dkQfG6\n애니어그램 테스트 https://t.co/Ml89EqRVVB ht…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "O_ZZ_U",
+					"name": "반❄ 오쯔",
+					"id": 4673089298,
+					"id_str": "4673089298",
+					"indices": [
+						3,
+						10
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/IB9NuHsoLy",
+					"expanded_url": "https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9-%EC%9C%A0%ED%98%95",
+					"display_url": "16personalities.com/ko/%EC%84%B1%E…",
+					"indices": [
+						46,
+						69
+					]
+				},
+				{
+					"url": "https://t.co/aXJ7dkQfG6",
+					"expanded_url": "http://egogramtest.kr/",
+					"display_url": "egogramtest.kr",
+					"indices": [
+						79,
+						102
+					]
+				},
+				{
+					"url": "https://t.co/Ml89EqRVVB",
+					"expanded_url": "https://enneagram-app.appspot.com/quest",
+					"display_url": "enneagram-app.appspot.com/quest",
+					"indices": [
+						113,
+						136
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 815064414,
+			"id_str": "815064414",
+			"name": "¿?",
+			"screen_name": "PF_Si2",
+			"location": "",
+			"description": "혼자 왔습니다",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 2,
+			"friends_count": 12,
+			"listed_count": 0,
+			"created_at": "Mon Sep 10 11:53:42 +0000 2012",
+			"favourites_count": 145,
+			"utc_offset": -36000,
+			"time_zone": "Hawaii",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1444,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/614830902074437632/owtHOm8x.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/614830902074437632/owtHOm8x.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/747209325135822848/jNfrA_Vm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/747209325135822848/jNfrA_Vm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/815064414/1466982374",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sun Sep 03 07:22:00 +0000 2017",
+			"id": 904243060657160200,
+			"id_str": "904243060657160192",
+			"text": "내가 신청서 쓸 때/자캐 짤 때 참고하는것들\nMBTI 테스트 https://t.co/IB9NuHsoLy\n에고그램 테스트 https://t.co/aXJ7dkQfG6\n애니어그램 테스트… https://t.co/yGEM3LHQBY",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/IB9NuHsoLy",
+						"expanded_url": "https://www.16personalities.com/ko/%EC%84%B1%EA%B2%A9-%EC%9C%A0%ED%98%95",
+						"display_url": "16personalities.com/ko/%EC%84%B1%E…",
+						"indices": [
+							34,
+							57
+						]
+					},
+					{
+						"url": "https://t.co/aXJ7dkQfG6",
+						"expanded_url": "http://egogramtest.kr/",
+						"display_url": "egogramtest.kr",
+						"indices": [
+							67,
+							90
+						]
+					},
+					{
+						"url": "https://t.co/yGEM3LHQBY",
+						"expanded_url": "https://twitter.com/i/web/status/904243060657160192",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							102,
+							125
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 4673089298,
+				"id_str": "4673089298",
+				"name": "반❄ 오쯔",
+				"screen_name": "O_ZZ_U",
+				"location": "이테 루이제 재민이 요한이 옆...",
+				"description": "마감중　/　0917~1225 요한이와 계연중 ♥♥♥  /  세일러문좋아함",
+				"url": "https://t.co/Gcn32pvzbq",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Gcn32pvzbq",
+								"expanded_url": "https://www.youtube.com/channel/UCLQwHqRzsBfuQdOt5RzB8iw",
+								"display_url": "youtube.com/channel/UCLQwH…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 512,
+				"friends_count": 138,
+				"listed_count": 3,
+				"created_at": "Tue Dec 29 13:57:38 +0000 2015",
+				"favourites_count": 4930,
+				"utc_offset": -25200,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 171,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/693502928724725761/Tc8rTBJy.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/693502928724725761/Tc8rTBJy.jpg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/917399519570010118/bYCORGA4_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/917399519570010118/bYCORGA4_normal.jpg",
+				"profile_link_color": "88DDFF",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5901,
+			"favorite_count": 4560,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 5901,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	},
+	{
 		"created_at": "Sat Oct 07 18:15:50 +0000 2017",
 		"id": 916728793091338200,
 		"id_str": "916728793091338240",
