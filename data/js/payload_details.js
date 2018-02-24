acd2f2e3ba38357fb28aed4773c2a10c0d08c9b8
ggongkui <32505785+ggongkui@users.noreply.github.com>
2018-02-24 09:15:31 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1504,
-	"created_at": "Mon Feb 19 00:15:29 +0000 2018",
+	"tweets": 1506,
+	"created_at": "Sat Feb 24 00:15:30 +0000 2018",
 	"lang": "ko"
 }
--- data/js/tweets/2018_02.js
+++ data/js/tweets/2018_02.js
@@ -1,6 +1,561 @@
 Grailbird.data.tweets_2018_02 = 
 [
 	{
+		"created_at": "Fri Feb 23 17:38:05 +0000 2018",
+		"id": 967091202741776400,
+		"id_str": "967091202741776384",
+		"text": "RT @IOWA_DUCKSAM: 글구 전 말풍선 그리는걸 못해서 남이 만들어놓은 소재 쓰는데 저 처럼 원 못 그리시는 분들은 쓰시면 좋을것같은... 근데 클리피군요\nhttps://t.co/R0ll68v9Qo https://t.co/kdxmfeTYgS",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "IOWA_DUCKSAM",
+					"name": "덕삼",
+					"id": 707412360240050200,
+					"id_str": "707412360240050176",
+					"indices": [
+						3,
+						16
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/R0ll68v9Qo",
+					"expanded_url": "https://assets.clip-studio.com/ja-jp/detail?id=1699148",
+					"display_url": "assets.clip-studio.com/ja-jp/detail?i…",
+					"indices": [
+						92,
+						115
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 966717496827920400,
+					"id_str": "966717496827920384",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/DWp4cQSVQAAZXai.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DWp4cQSVQAAZXai.jpg",
+					"url": "https://t.co/kdxmfeTYgS",
+					"display_url": "pic.twitter.com/kdxmfeTYgS",
+					"expanded_url": "https://twitter.com/IOWA_DUCKSAM/status/966717730245132288/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1402,
+							"h": 996,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 852,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 483,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 966717730245132300,
+					"source_status_id_str": "966717730245132288",
+					"source_user_id": 707412360240050200,
+					"source_user_id_str": "707412360240050176"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 966717496827920400,
+					"id_str": "966717496827920384",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/DWp4cQSVQAAZXai.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DWp4cQSVQAAZXai.jpg",
+					"url": "https://t.co/kdxmfeTYgS",
+					"display_url": "pic.twitter.com/kdxmfeTYgS",
+					"expanded_url": "https://twitter.com/IOWA_DUCKSAM/status/966717730245132288/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1402,
+							"h": 996,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 852,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 483,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 966717730245132300,
+					"source_status_id_str": "966717730245132288",
+					"source_user_id": 707412360240050200,
+					"source_user_id_str": "707412360240050176"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 815064414,
+			"id_str": "815064414",
+			"name": "¿?",
+			"screen_name": "PF_Si2",
+			"location": "",
+			"description": "혼자 왔습니다",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 2,
+			"friends_count": 12,
+			"listed_count": 0,
+			"created_at": "Mon Sep 10 11:53:42 +0000 2012",
+			"favourites_count": 142,
+			"utc_offset": -36000,
+			"time_zone": "Hawaii",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1476,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/614830902074437632/owtHOm8x.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/614830902074437632/owtHOm8x.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/747209325135822848/jNfrA_Vm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/747209325135822848/jNfrA_Vm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/815064414/1466982374",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Feb 22 16:54:02 +0000 2018",
+			"id": 966717730245132300,
+			"id_str": "966717730245132288",
+			"text": "글구 전 말풍선 그리는걸 못해서 남이 만들어놓은 소재 쓰는데 저 처럼 원 못 그리시는 분들은 쓰시면 좋을것같은... 근데 클리피군요\nhttps://t.co/R0ll68v9Qo https://t.co/kdxmfeTYgS",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/R0ll68v9Qo",
+						"expanded_url": "https://assets.clip-studio.com/ja-jp/detail?id=1699148",
+						"display_url": "assets.clip-studio.com/ja-jp/detail?i…",
+						"indices": [
+							74,
+							97
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 966717496827920400,
+						"id_str": "966717496827920384",
+						"indices": [
+							98,
+							121
+						],
+						"media_url": "http://pbs.twimg.com/media/DWp4cQSVQAAZXai.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DWp4cQSVQAAZXai.jpg",
+						"url": "https://t.co/kdxmfeTYgS",
+						"display_url": "pic.twitter.com/kdxmfeTYgS",
+						"expanded_url": "https://twitter.com/IOWA_DUCKSAM/status/966717730245132288/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1402,
+								"h": 996,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 852,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 483,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 966717496827920400,
+						"id_str": "966717496827920384",
+						"indices": [
+							98,
+							121
+						],
+						"media_url": "http://pbs.twimg.com/media/DWp4cQSVQAAZXai.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DWp4cQSVQAAZXai.jpg",
+						"url": "https://t.co/kdxmfeTYgS",
+						"display_url": "pic.twitter.com/kdxmfeTYgS",
+						"expanded_url": "https://twitter.com/IOWA_DUCKSAM/status/966717730245132288/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1402,
+								"h": 996,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 852,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 483,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": 966717011765047300,
+			"in_reply_to_status_id_str": "966717011765047297",
+			"in_reply_to_user_id": 707412360240050200,
+			"in_reply_to_user_id_str": "707412360240050176",
+			"in_reply_to_screen_name": "IOWA_DUCKSAM",
+			"user": {
+				"id": 707412360240050200,
+				"id_str": "707412360240050176",
+				"name": "덕삼",
+				"screen_name": "IOWA_DUCKSAM",
+				"location": "iowa",
+				"description": "올캐러 리버시블 문의는 ☞ (@ducksam_E)",
+				"url": "https://t.co/4D0z2LPPK5",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/4D0z2LPPK5",
+								"expanded_url": "http://22330104.tistory.com/",
+								"display_url": "22330104.tistory.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 6160,
+				"friends_count": 313,
+				"listed_count": 74,
+				"created_at": "Wed Mar 09 03:47:04 +0000 2016",
+				"favourites_count": 21915,
+				"utc_offset": -28800,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 299,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/709232319089934336/Gbc7BBcl.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/709232319089934336/Gbc7BBcl.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/963727885621080064/eG87nrIC_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/963727885621080064/eG87nrIC_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/707412360240050176/1516552414",
+				"profile_link_color": "FAB81E",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 122,
+			"favorite_count": 124,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 122,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	},
+	{
+		"created_at": "Fri Feb 23 09:08:17 +0000 2018",
+		"id": 966962908696424400,
+		"id_str": "966962908696424448",
+		"text": "https://t.co/e8W0kSSE6S",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/e8W0kSSE6S",
+					"expanded_url": "https://twitter.com/bongy_vw/status/966691074218455040",
+					"display_url": "twitter.com/bongy_vw/statu…",
+					"indices": [
+						0,
+						23
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 815064414,
+			"id_str": "815064414",
+			"name": "¿?",
+			"screen_name": "PF_Si2",
+			"location": "",
+			"description": "혼자 왔습니다",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 2,
+			"friends_count": 12,
+			"listed_count": 0,
+			"created_at": "Mon Sep 10 11:53:42 +0000 2012",
+			"favourites_count": 142,
+			"utc_offset": -36000,
+			"time_zone": "Hawaii",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1476,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/614830902074437632/owtHOm8x.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/614830902074437632/owtHOm8x.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/747209325135822848/jNfrA_Vm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/747209325135822848/jNfrA_Vm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/815064414/1466982374",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 966691074218455000,
+		"quoted_status_id_str": "966691074218455040",
+		"quoted_status": {
+			"created_at": "Thu Feb 22 15:08:07 +0000 2018",
+			"id": 966691074218455000,
+			"id_str": "966691074218455040",
+			"text": "CoC 시나리오 &lt;복숭아 소년&gt; 배포합니다. 학교를 배경으로 한 타이만 시나리오이며, 서로 애정을 가지고 있는 사이에게 추천합니다. 자/앤캐의 학교 AU에도 적합합니다. 단 본 세션에는 유혈, 식인, 좀비를… https://t.co/qKozySz0gP",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/qKozySz0gP",
+						"expanded_url": "https://twitter.com/i/web/status/966691074218455040",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							123,
+							146
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 927610206795599900,
+				"id_str": "927610206795599872",
+				"name": "oO(커미션 중) 봉양",
+				"screen_name": "bongY_vW",
+				"location": "",
+				"description": "안녕, 내사랑. 혹시 찡그리면 웃어줘야지. @ur_wood",
+				"url": "https://t.co/i2Tcw5DOru",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/i2Tcw5DOru",
+								"expanded_url": "https://thak7193.wixsite.com/commission",
+								"display_url": "thak7193.wixsite.com/commission",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 100,
+				"friends_count": 85,
+				"listed_count": 2,
+				"created_at": "Mon Nov 06 18:54:42 +0000 2017",
+				"favourites_count": 3867,
+				"utc_offset": 32400,
+				"time_zone": "Seoul",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 23987,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/964723495895629825/4xppUT8G_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/964723495895629825/4xppUT8G_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/927610206795599872/1518766451",
+				"profile_link_color": "BC8F8F",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1473,
+			"favorite_count": 619,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und"
+	},
+	{
 		"created_at": "Sun Feb 18 18:35:50 +0000 2018",
 		"id": 965293798564577300,
 		"id_str": "965293798564577280",
