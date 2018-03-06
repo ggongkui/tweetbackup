d63dbf2bb91438355d736718661bc2d665b8287d
ggongkui <32505785+ggongkui@users.noreply.github.com>
2018-03-06 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1507,
-	"created_at": "Sun Feb 25 00:15:29 +0000 2018",
+	"tweets": 1508,
+	"created_at": "Tue Mar 06 00:15:29 +0000 2018",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2018_03.js
@@ -0,0 +1,199 @@
+Grailbird.data.tweets_2018_03 = 
+[
+	{
+		"created_at": "Mon Mar 05 12:52:52 +0000 2018",
+		"id": 970643305028468700,
+		"id_str": "970643305028468736",
+		"text": "RT @x____w_swuw: https://t.co/t5IRE5fKfB 유용해 보이는 소재. 체크무늬는 실제론 천 재질에 사용될 때가 많은데, 많은 체크소재가 반듯한 형태다보니 일일이 수정해 줘야 하는 번거로움이 있었지만 이건 그걸 생략해 준 형태…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "x____w_swuw",
+					"name": "🏴 깃발의 영원이 8일째 🏴",
+					"id": 850776213144559600,
+					"id_str": "850776213144559616",
+					"indices": [
+						3,
+						15
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/t5IRE5fKfB",
+					"expanded_url": "https://assets.clip-studio.com/ja-jp/detail?id=1700133",
+					"display_url": "assets.clip-studio.com/ja-jp/detail?i…",
+					"indices": [
+						17,
+						40
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 815064414,
+			"id_str": "815064414",
+			"name": "¿?",
+			"screen_name": "PF_Si2",
+			"location": "",
+			"description": "혼자 왔습니다",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 2,
+			"friends_count": 12,
+			"listed_count": 0,
+			"created_at": "Mon Sep 10 11:53:42 +0000 2012",
+			"favourites_count": 142,
+			"utc_offset": -36000,
+			"time_zone": "Hawaii",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1477,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/614830902074437632/owtHOm8x.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/614830902074437632/owtHOm8x.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/747209325135822848/jNfrA_Vm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/747209325135822848/jNfrA_Vm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/815064414/1466982374",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Dec 30 06:53:56 +0000 2017",
+			"id": 946997765543882800,
+			"id_str": "946997765543882752",
+			"text": "https://t.co/t5IRE5fKfB 유용해 보이는 소재. 체크무늬는 실제론 천 재질에 사용될 때가 많은데, 많은 체크소재가 반듯한 형태다보니 일일이 수정해 줘야 하는 번거로움이 있었지만 이건 그걸 생… https://t.co/qOYREv037O",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/t5IRE5fKfB",
+						"expanded_url": "https://assets.clip-studio.com/ja-jp/detail?id=1700133",
+						"display_url": "assets.clip-studio.com/ja-jp/detail?i…",
+						"indices": [
+							0,
+							23
+						]
+					},
+					{
+						"url": "https://t.co/qOYREv037O",
+						"expanded_url": "https://twitter.com/i/web/status/946997765543882752",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": 946996436331249700,
+			"in_reply_to_status_id_str": "946996436331249666",
+			"in_reply_to_user_id": 850776213144559600,
+			"in_reply_to_user_id_str": "850776213144559616",
+			"in_reply_to_screen_name": "x____w_swuw",
+			"user": {
+				"id": 850776213144559600,
+				"id_str": "850776213144559616",
+				"name": "🏴 깃발의 영원이 9일째 🏴",
+				"screen_name": "x____w_swuw",
+				"location": "",
+				"description": "나는 나이를 먹었어. 하루에 하루씩, 한 달에 한 달씩. 한 해에 한 살씩, 시간을 몸에 쌓으며 살았어. 그러니까 나는 당신에게 어울리는 사람이야. - 당신을 기다리고 있어 中",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 211,
+				"friends_count": 306,
+				"listed_count": 2,
+				"created_at": "Sat Apr 08 18:23:50 +0000 2017",
+				"favourites_count": 2932,
+				"utc_offset": 32400,
+				"time_zone": "Seoul",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 25008,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/970232496263544833/5z0FeNYW_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/970232496263544833/5z0FeNYW_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/850776213144559616/1518522608",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7403,
+			"favorite_count": 5062,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 7403,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	}
+]
