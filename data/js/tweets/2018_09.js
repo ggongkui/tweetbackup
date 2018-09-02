e03f123a0d6f54b39c6835f6d1cb58186807d8c0
ggongkui <32505785+ggongkui@users.noreply.github.com>
2018-09-02 09:15:30 +0900

    Added 1 tweets

---
+++ data/js/tweets/2018_09.js
@@ -0,0 +1,444 @@
+Grailbird.data.tweets_2018_09 = 
+[
+	{
+		"created_at": "Sat Sep 01 02:21:00 +0000 2018",
+		"id": 1035714106290921500,
+		"id_str": "1035714106290921472",
+		"text": "RT @m_yosry2012: Gentle doggo https://t.co/o8RfXzLUci",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "m_yosry2012",
+					"name": "Gaml .y",
+					"id": 2327807249,
+					"id_str": "2327807249",
+					"indices": [
+						3,
+						15
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1035311140136468500,
+					"id_str": "1035311140136468480",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1035311140136468480/pu/img/ba7Tz-3FUmo-O4nd.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1035311140136468480/pu/img/ba7Tz-3FUmo-O4nd.jpg",
+					"url": "https://t.co/o8RfXzLUci",
+					"display_url": "pic.twitter.com/o8RfXzLUci",
+					"expanded_url": "https://twitter.com/m_yosry2012/status/1035311259774840832/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1035311259774840800,
+					"source_status_id_str": "1035311259774840832",
+					"source_user_id": 2327807249,
+					"source_user_id_str": "2327807249"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1035311140136468500,
+					"id_str": "1035311140136468480",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1035311140136468480/pu/img/ba7Tz-3FUmo-O4nd.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1035311140136468480/pu/img/ba7Tz-3FUmo-O4nd.jpg",
+					"url": "https://t.co/o8RfXzLUci",
+					"display_url": "pic.twitter.com/o8RfXzLUci",
+					"expanded_url": "https://twitter.com/m_yosry2012/status/1035311259774840832/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1035311259774840800,
+					"source_status_id_str": "1035311259774840832",
+					"source_user_id": 2327807249,
+					"source_user_id_str": "2327807249",
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							5
+						],
+						"duration_millis": 17233,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1035311140136468480/pu/vid/180x224/7Txbj2bK95A8NYRo.mp4?tag=5"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1035311140136468480/pu/vid/720x900/3c-avwg8zi9VyFuv.mp4?tag=5"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1035311140136468480/pu/vid/360x450/-lqXZ5sO8c2En3RF.mp4?tag=5"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1035311140136468480/pu/pl/gVaZiEV7vt7ko5Dq.m3u8?tag=5"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false,
+						"source_user": {
+							"id": 2327807249,
+							"id_str": "2327807249",
+							"name": "Gaml .y",
+							"screen_name": "m_yosry2012",
+							"location": "",
+							"description": "Executive Director\nI love nature, simplicity ,, animals\npl no group,no DM 😎",
+							"url": null,
+							"entities": {
+								"description": {
+									"urls": []
+								}
+							},
+							"protected": false,
+							"followers_count": 106452,
+							"friends_count": 12321,
+							"listed_count": 1850,
+							"created_at": "Thu Feb 06 09:51:45 +0000 2014",
+							"favourites_count": 191188,
+							"utc_offset": null,
+							"time_zone": null,
+							"geo_enabled": true,
+							"verified": false,
+							"statuses_count": 224262,
+							"lang": "ar",
+							"contributors_enabled": false,
+							"is_translator": false,
+							"is_translation_enabled": false,
+							"profile_background_color": "C0DEED",
+							"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+							"profile_background_tile": false,
+							"profile_image_url": "http://pbs.twimg.com/profile_images/616371452602904576/a2zdPJcN_normal.jpg",
+							"profile_image_url_https": "https://pbs.twimg.com/profile_images/616371452602904576/a2zdPJcN_normal.jpg",
+							"profile_banner_url": "https://pbs.twimg.com/profile_banners/2327807249/1524854570",
+							"profile_link_color": "1DA1F2",
+							"profile_sidebar_border_color": "C0DEED",
+							"profile_sidebar_fill_color": "DDEEF6",
+							"profile_text_color": "333333",
+							"profile_use_background_image": true,
+							"has_extended_profile": false,
+							"default_profile": true,
+							"default_profile_image": false,
+							"following": false,
+							"follow_request_sent": false,
+							"notifications": false,
+							"translator_type": "none"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 815064414,
+			"id_str": "815064414",
+			"name": "¿?",
+			"screen_name": "PF_Si2",
+			"location": "",
+			"description": "나는 재앙을 불러들였고, 그리하여 모래와 피로 숨이 막혔다. 불행은 나의 신이었다. / 아르튀르 랭보, 지옥에서 보내는 한철",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 1,
+			"friends_count": 1,
+			"listed_count": 0,
+			"created_at": "Mon Sep 10 11:53:42 +0000 2012",
+			"favourites_count": 142,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1558,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/981236962622193664/YPhFNja3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/981236962622193664/YPhFNja3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/815064414/1466982374",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Aug 30 23:40:14 +0000 2018",
+			"id": 1035311259774840800,
+			"id_str": "1035311259774840832",
+			"text": "Gentle doggo https://t.co/o8RfXzLUci",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1035311140136468500,
+						"id_str": "1035311140136468480",
+						"indices": [
+							13,
+							36
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1035311140136468480/pu/img/ba7Tz-3FUmo-O4nd.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1035311140136468480/pu/img/ba7Tz-3FUmo-O4nd.jpg",
+						"url": "https://t.co/o8RfXzLUci",
+						"display_url": "pic.twitter.com/o8RfXzLUci",
+						"expanded_url": "https://twitter.com/m_yosry2012/status/1035311259774840832/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 544,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 960,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 960,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1035311140136468500,
+						"id_str": "1035311140136468480",
+						"indices": [
+							13,
+							36
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1035311140136468480/pu/img/ba7Tz-3FUmo-O4nd.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1035311140136468480/pu/img/ba7Tz-3FUmo-O4nd.jpg",
+						"url": "https://t.co/o8RfXzLUci",
+						"display_url": "pic.twitter.com/o8RfXzLUci",
+						"expanded_url": "https://twitter.com/m_yosry2012/status/1035311259774840832/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 544,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 960,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 960,
+								"h": 1200,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								4,
+								5
+							],
+							"duration_millis": 17233,
+							"variants": [
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1035311140136468480/pu/vid/180x224/7Txbj2bK95A8NYRo.mp4?tag=5"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1035311140136468480/pu/vid/720x900/3c-avwg8zi9VyFuv.mp4?tag=5"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1035311140136468480/pu/vid/360x450/-lqXZ5sO8c2En3RF.mp4?tag=5"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1035311140136468480/pu/pl/gVaZiEV7vt7ko5Dq.m3u8?tag=5"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2327807249,
+				"id_str": "2327807249",
+				"name": "Gaml .y",
+				"screen_name": "m_yosry2012",
+				"location": "",
+				"description": "Executive Director\nI love nature, simplicity ,, animals\npl no group,no DM 😎",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 106452,
+				"friends_count": 12321,
+				"listed_count": 1850,
+				"created_at": "Thu Feb 06 09:51:45 +0000 2014",
+				"favourites_count": 191188,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 224262,
+				"lang": "ar",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/616371452602904576/a2zdPJcN_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/616371452602904576/a2zdPJcN_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2327807249/1524854570",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 15717,
+			"favorite_count": 30165,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 15717,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	}
+]
