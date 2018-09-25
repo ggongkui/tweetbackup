11aec99919d0f8791eb7ea1ad91947216681213c
ggongkui <32505785+ggongkui@users.noreply.github.com>
2018-09-25 09:15:31 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1658,
-	"created_at": "Sat Sep 22 00:15:29 +0000 2018",
+	"tweets": 1659,
+	"created_at": "Tue Sep 25 00:15:30 +0000 2018",
 	"lang": "ko"
 }
--- data/js/tweets/2018_09.js
+++ data/js/tweets/2018_09.js
@@ -1,6 +1,197 @@
 Grailbird.data.tweets_2018_09 = 
 [
 	{
+		"created_at": "Mon Sep 24 08:38:10 +0000 2018",
+		"id": 1044143944177598500,
+		"id_str": "1044143944177598464",
+		"text": "https://t.co/YTwM3Qicxb",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YTwM3Qicxb",
+					"expanded_url": "https://twitter.com/uro_design/status/1035399163511373829",
+					"display_url": "twitter.com/uro_design/sta…",
+					"indices": [
+						0,
+						23
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 815064414,
+			"id_str": "815064414",
+			"name": "¿?",
+			"screen_name": "PF_Si2",
+			"location": "",
+			"description": "나는 재앙을 불러들였고, 그리하여 모래와 피로 숨이 막혔다. 불행은 나의 신이었다. / 아르튀르 랭보, 지옥에서 보내는 한철",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 1,
+			"friends_count": 1,
+			"listed_count": 0,
+			"created_at": "Mon Sep 10 11:53:42 +0000 2012",
+			"favourites_count": 138,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1526,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/981236962622193664/YPhFNja3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/981236962622193664/YPhFNja3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/815064414/1466982374",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1035399163511373800,
+		"quoted_status_id_str": "1035399163511373829",
+		"quoted_status": {
+			"created_at": "Fri Aug 31 05:29:32 +0000 2018",
+			"id": 1035399163511373800,
+			"id_str": "1035399163511373829",
+			"text": "[동인지 표지 19금 마크]\n옛날에 만들었던 19금 마크를 리디자인하여 배포합니다. \n자유롭게 색 변경&amp;리사이징해서 사용하세요. 상업적 이용OK\n대응 확장자 : AI(cs5이상), PSD, PNG\n다운로드… https://t.co/uW6911yPlo",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/uW6911yPlo",
+						"expanded_url": "https://twitter.com/i/web/status/1035399163511373829",
+						"display_url": "twitter.com/i/web/status/1…",
+						"indices": [
+							120,
+							143
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3562208905,
+				"id_str": "3562208905",
+				"name": "URO DESIGN",
+				"screen_name": "uro_design",
+				"location": "urojo@naver.com",
+				"description": "디자이너 유로입니다. 로고 제작, 표지 편집, 전단지 편집, 이벤트 기획 이미지를 제작하고 관련 잡담도 합니다. 문의에 답변이 늦을 수 있습니다. 상세 안내 및 샘플은 링크↓↓",
+				"url": "https://t.co/BS0gMjFwU7",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/BS0gMjFwU7",
+								"expanded_url": "http://urojo.blog.me/80208800400",
+								"display_url": "urojo.blog.me/80208800400",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4458,
+				"friends_count": 55,
+				"listed_count": 94,
+				"created_at": "Mon Sep 14 16:46:42 +0000 2015",
+				"favourites_count": 398,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 815,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/804045606008209408/BJ5WyM1Z_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/804045606008209408/BJ5WyM1Z_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3562208905/1442252401",
+				"profile_link_color": "FF5C5D",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3921,
+			"favorite_count": 2509,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und"
+	},
+	{
 		"created_at": "Fri Sep 21 04:56:42 +0000 2018",
 		"id": 1043001047801688000,
 		"id_str": "1043001047801688064",
