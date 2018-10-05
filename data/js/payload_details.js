4300effeac4b35f820d2c864914c7f29a201ceb6
ggongkui <32505785+ggongkui@users.noreply.github.com>
2018-10-05 09:15:31 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1659,
-	"created_at": "Tue Sep 25 00:15:30 +0000 2018",
+	"tweets": 1660,
+	"created_at": "Fri Oct 05 00:15:30 +0000 2018",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2018_10.js
@@ -0,0 +1,347 @@
+Grailbird.data.tweets_2018_10 = 
+[
+	{
+		"created_at": "Thu Oct 04 15:50:31 +0000 2018",
+		"id": 1047876630566166500,
+		"id_str": "1047876630566166528",
+		"text": "RT @reading_bot_: 내 손을 잡아줄래요?\n피하지 말고 피하지 말고\n내가 왜 당신을 사랑할 수밖에 없는지\n그 막연함도 들어볼래요?\n(허수경, 내 손을 잡아줄래요?)\n\n고인의 명복을 빕니다. https://t.co/uIwk9zqqKj",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "reading_bot_",
+					"name": "독서봇🌈",
+					"id": 334250559,
+					"id_str": "334250559",
+					"indices": [
+						3,
+						16
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1047618574871748600,
+					"id_str": "1047618574871748608",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/DonjixdU4AAIB-3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DonjixdU4AAIB-3.jpg",
+					"url": "https://t.co/uIwk9zqqKj",
+					"display_url": "pic.twitter.com/uIwk9zqqKj",
+					"expanded_url": "https://twitter.com/reading_bot_/status/1047618583235153920/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 445,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1122,
+							"h": 734,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1122,
+							"h": 734,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1047618583235153900,
+					"source_status_id_str": "1047618583235153920",
+					"source_user_id": 334250559,
+					"source_user_id_str": "334250559"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1047618574871748600,
+					"id_str": "1047618574871748608",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/DonjixdU4AAIB-3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DonjixdU4AAIB-3.jpg",
+					"url": "https://t.co/uIwk9zqqKj",
+					"display_url": "pic.twitter.com/uIwk9zqqKj",
+					"expanded_url": "https://twitter.com/reading_bot_/status/1047618583235153920/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 445,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1122,
+							"h": 734,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1122,
+							"h": 734,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1047618583235153900,
+					"source_status_id_str": "1047618583235153920",
+					"source_user_id": 334250559,
+					"source_user_id_str": "334250559"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 815064414,
+			"id_str": "815064414",
+			"name": "¿?",
+			"screen_name": "PF_Si2",
+			"location": "",
+			"description": "나는 재앙을 불러들였고, 그리하여 모래와 피로 숨이 막혔다. 불행은 나의 신이었다. / 아르튀르 랭보, 지옥에서 보내는 한철",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 1,
+			"friends_count": 1,
+			"listed_count": 0,
+			"created_at": "Mon Sep 10 11:53:42 +0000 2012",
+			"favourites_count": 138,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1524,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/981236962622193664/YPhFNja3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/981236962622193664/YPhFNja3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/815064414/1466982374",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Oct 03 22:45:08 +0000 2018",
+			"id": 1047618583235153900,
+			"id_str": "1047618583235153920",
+			"text": "내 손을 잡아줄래요?\n피하지 말고 피하지 말고\n내가 왜 당신을 사랑할 수밖에 없는지\n그 막연함도 들어볼래요?\n(허수경, 내 손을 잡아줄래요?)\n\n고인의 명복을 빕니다. https://t.co/uIwk9zqqKj",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1047618574871748600,
+						"id_str": "1047618574871748608",
+						"indices": [
+							94,
+							117
+						],
+						"media_url": "http://pbs.twimg.com/media/DonjixdU4AAIB-3.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DonjixdU4AAIB-3.jpg",
+						"url": "https://t.co/uIwk9zqqKj",
+						"display_url": "pic.twitter.com/uIwk9zqqKj",
+						"expanded_url": "https://twitter.com/reading_bot_/status/1047618583235153920/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 445,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1122,
+								"h": 734,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1122,
+								"h": 734,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1047618574871748600,
+						"id_str": "1047618574871748608",
+						"indices": [
+							94,
+							117
+						],
+						"media_url": "http://pbs.twimg.com/media/DonjixdU4AAIB-3.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DonjixdU4AAIB-3.jpg",
+						"url": "https://t.co/uIwk9zqqKj",
+						"display_url": "pic.twitter.com/uIwk9zqqKj",
+						"expanded_url": "https://twitter.com/reading_bot_/status/1047618583235153920/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 445,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1122,
+								"h": 734,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1122,
+								"h": 734,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 334250559,
+				"id_str": "334250559",
+				"name": "독서봇🌈",
+				"screen_name": "reading_bot_",
+				"location": "책과 책 사이에 당신의 공간. ",
+				"description": "#인용RT시 #블락. 어느 한 개인의 독서 메모장. 책 추천은 전문가에게. 읽은 책 중, 운영자 개인의 취향에 따른 단편적인 글을 올립니다. 그래서 편협할 수 있습니다. 그러니, '당신이 직접 책을 읽었으면 좋겠습니다.' 질문은 @reading_bot_2 #출처첨부",
+				"url": "https://t.co/DIASZEipnp",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/DIASZEipnp",
+								"expanded_url": "https://twitter.com/reading_bot_2",
+								"display_url": "twitter.com/reading_bot_2",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 469799,
+				"friends_count": 175,
+				"listed_count": 1294,
+				"created_at": "Tue Jul 12 20:13:36 +0000 2011",
+				"favourites_count": 647,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 10061,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FAF7FA",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/615177671069175808/m33zRmdD_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/615177671069175808/m33zRmdD_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/334250559/1410371363",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 645,
+			"favorite_count": 436,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 645,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	}
+]
