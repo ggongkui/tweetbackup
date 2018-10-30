411ecbd4b354151fedb94774b7aca14c4b47a254
ggongkui <32505785+ggongkui@users.noreply.github.com>
2018-10-30 09:15:41 +0900

    Added 335 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3591,
-	"created_at": "Mon Oct 29 00:15:30 +0000 2018",
+	"tweets": 3926,
+	"created_at": "Tue Oct 30 00:15:39 +0000 2018",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -1,5 +1,166 @@
 var tweet_index = [
 	{
+		"file_name": "data/js/tweets/2018_09.js",
+		"year": 2018,
+		"var_name": "tweets_2018_09",
+		"tweet_count": 14,
+		"month": 9
+	},
+	{
+		"file_name": "data/js/tweets/2018_08.js",
+		"year": 2018,
+		"var_name": "tweets_2018_08",
+		"tweet_count": 10,
+		"month": 8
+	},
+	{
+		"file_name": "data/js/tweets/2018_07.js",
+		"year": 2017,
+		"var_name": "tweets_2018_07",
+		"tweet_count": 13,
+		"month": 12
+	},
+	{
+		"file_name": "data/js/tweets/2018_06.js",
+		"year": 2018,
+		"var_name": "tweets_2018_06",
+		"tweet_count": 7,
+		"month": 6
+	},
+	{
+		"file_name": "data/js/tweets/2018_05.js",
+		"year": 2018,
+		"var_name": "tweets_2018_05",
+		"tweet_count": 11,
+		"month": 4
+	},
+	{
+		"file_name": "data/js/tweets/2018_04.js",
+		"year": 2017,
+		"var_name": "tweets_2018_04",
+		"tweet_count": 17,
+		"month": 3
+	},
+	{
+		"file_name": "data/js/tweets/2018_03.js",
+		"year": 2018,
+		"var_name": "tweets_2018_03",
+		"tweet_count": 16,
+		"month": 3
+	},
+	{
+		"file_name": "data/js/tweets/2018_02.js",
+		"year": 2018,
+		"var_name": "tweets_2018_02",
+		"tweet_count": 3,
+		"month": 2
+	},
+	{
+		"file_name": "data/js/tweets/2018_01.js",
+		"year": 2018,
+		"var_name": "tweets_2018_01",
+		"tweet_count": 17,
+		"month": 1
+	},
+	{
+		"file_name": "data/js/tweets/2017_12.js",
+		"year": 2017,
+		"var_name": "tweets_2017_12",
+		"tweet_count": 9,
+		"month": 12
+	},
+	{
+		"file_name": "data/js/tweets/2017_11.js",
+		"year": 2017,
+		"var_name": "tweets_2017_11",
+		"tweet_count": 9,
+		"month": 11
+	},
+	{
+		"file_name": "data/js/tweets/2017_10.js",
+		"year": 2017,
+		"var_name": "tweets_2017_10",
+		"tweet_count": 5,
+		"month": 10
+	},
+	{
+		"file_name": "data/js/tweets/2017_09.js",
+		"year": 2017,
+		"var_name": "tweets_2017_09",
+		"tweet_count": 11,
+		"month": 9
+	},
+	{
+		"file_name": "data/js/tweets/2017_08.js",
+		"year": 2017,
+		"var_name": "tweets_2017_08",
+		"tweet_count": 22,
+		"month": 8
+	},
+	{
+		"file_name": "data/js/tweets/2017_07.js",
+		"year": 2017,
+		"var_name": "tweets_2017_07",
+		"tweet_count": 12,
+		"month": 7
+	},
+	{
+		"file_name": "data/js/tweets/2017_06.js",
+		"year": 2016,
+		"var_name": "tweets_2017_06",
+		"tweet_count": 10,
+		"month": 10
+	},
+	{
+		"file_name": "data/js/tweets/2017_05.js",
+		"year": 2015,
+		"var_name": "tweets_2017_05",
+		"tweet_count": 5,
+		"month": 7
+	},
+	{
+		"file_name": "data/js/tweets/2017_04.js",
+		"year": 2017,
+		"var_name": "tweets_2017_04",
+		"tweet_count": 5,
+		"month": 4
+	},
+	{
+		"file_name": "data/js/tweets/2017_03.js",
+		"year": 2017,
+		"var_name": "tweets_2017_03",
+		"tweet_count": 15,
+		"month": 3
+	},
+	{
+		"file_name": "data/js/tweets/2017_02.js",
+		"year": 2017,
+		"var_name": "tweets_2017_02",
+		"tweet_count": 24,
+		"month": 2
+	},
+	{
+		"file_name": "data/js/tweets/2017_01.js",
+		"year": 2017,
+		"var_name": "tweets_2017_01",
+		"tweet_count": 24,
+		"month": 1
+	},
+	{
+		"file_name": "data/js/tweets/2016_12.js",
+		"year": 2016,
+		"var_name": "tweets_2016_12",
+		"tweet_count": 38,
+		"month": 12
+	},
+	{
+		"file_name": "data/js/tweets/2016_11.js",
+		"year": 2016,
+		"var_name": "tweets_2016_11",
+		"tweet_count": 37,
+		"month": 11
+	},
+	{
 		"file_name": "data/js/tweets/2018_10.js",
 		"year": 2016,
 		"var_name": "tweets_2018_10",
