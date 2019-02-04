9628f9910efa918e4c93afc944f1fca8ec9f7ff3
ggongkui <32505785+ggongkui@users.noreply.github.com>
2019-02-04 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8564,
-	"created_at": "Sat Jan 12 00:15:30 +0000 2019",
+	"tweets": 8565,
+	"created_at": "Mon Feb 04 00:15:29 +0000 2019",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2019_02.js
@@ -0,0 +1,294 @@
+Grailbird.data.tweets_2019_02 = 
+[
+	{
+		"created_at": "Wed Aug 23 10:34:52 +0000 2017",
+		"id": 900305330545897500,
+		"id_str": "1092144395011293184",
+		"full_text": "#BL画像 #Mods #NatsumeKazuki https://t.co/oqtNvaIx3M",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			26
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BL画像",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"text": "Mods",
+					"indices": [
+						6,
+						11
+					]
+				},
+				{
+					"text": "NatsumeKazuki",
+					"indices": [
+						12,
+						26
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 900305263814615000,
+					"id_str": "900305263814615040",
+					"indices": [
+						27,
+						50
+					],
+					"media_url": "http://pbs.twimg.com/media/DH6G3g8VoAAya4p.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DH6G3g8VoAAya4p.jpg",
+					"url": "https://t.co/oqtNvaIx3M",
+					"display_url": "pic.twitter.com/oqtNvaIx3M",
+					"expanded_url": "https://twitter.com/bl_iceblog/status/900305330545897473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 799,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 850,
+							"h": 1277,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 900305263814615000,
+					"id_str": "900305263814615040",
+					"indices": [
+						27,
+						50
+					],
+					"media_url": "http://pbs.twimg.com/media/DH6G3g8VoAAya4p.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DH6G3g8VoAAya4p.jpg",
+					"url": "https://t.co/oqtNvaIx3M",
+					"display_url": "pic.twitter.com/oqtNvaIx3M",
+					"expanded_url": "https://twitter.com/bl_iceblog/status/900305330545897473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 799,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 850,
+							"h": 1277,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 900305282877734900,
+					"id_str": "900305282877734912",
+					"indices": [
+						27,
+						50
+					],
+					"media_url": "http://pbs.twimg.com/media/DH6G4n9VwAA5sGi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DH6G4n9VwAA5sGi.jpg",
+					"url": "https://t.co/oqtNvaIx3M",
+					"display_url": "pic.twitter.com/oqtNvaIx3M",
+					"expanded_url": "https://twitter.com/bl_iceblog/status/900305330545897473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 479,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 846,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 1277,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 900305298123964400,
+					"id_str": "900305298123964416",
+					"indices": [
+						27,
+						50
+					],
+					"media_url": "http://pbs.twimg.com/media/DH6G5gwUwAAwWsu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DH6G5gwUwAAwWsu.jpg",
+					"url": "https://t.co/oqtNvaIx3M",
+					"display_url": "pic.twitter.com/oqtNvaIx3M",
+					"expanded_url": "https://twitter.com/bl_iceblog/status/900305330545897473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 809,
+							"h": 1277,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 431,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 760,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 900305311499669500,
+					"id_str": "900305311499669509",
+					"indices": [
+						27,
+						50
+					],
+					"media_url": "http://pbs.twimg.com/media/DH6G6SlV0AU6h4_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DH6G6SlV0AU6h4_.jpg",
+					"url": "https://t.co/oqtNvaIx3M",
+					"display_url": "pic.twitter.com/oqtNvaIx3M",
+					"expanded_url": "https://twitter.com/bl_iceblog/status/900305330545897473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 809,
+							"h": 1187,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 809,
+							"h": 1187,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 463,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3193642992,
+			"id_str": "3193642992",
+			"name": "※ BL画像bot ※",
+			"screen_name": "bl_iceblog",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 38047,
+			"friends_count": 8,
+			"listed_count": 305,
+			"created_at": "Tue May 12 22:53:07 +0000 2015",
+			"favourites_count": 2082,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10309,
+			"lang": "ja",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/676534543575158784/j9Fdje57_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/676534543575158784/j9Fdje57_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3193642992/1457837132",
+			"profile_link_color": "FFCC4D",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 199,
+		"favorite_count": 596,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "#BL画像 #Mods #NatsumeKazuki https://t.co/oqtNvaIx3M"
+	}
+]
