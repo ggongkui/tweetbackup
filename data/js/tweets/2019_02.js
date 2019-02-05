0e8a41c4e817ff73472f0626de343b789778df1f
ggongkui <32505785+ggongkui@users.noreply.github.com>
2019-02-05 09:15:30 +0900

    Added 2 tweets

--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,354 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Fri Feb 01 10:34:28 +0000 2019",
+		"id": 1091283635452338200,
+		"id_str": "1092550969965985792",
+		"full_text": "지인분이 물어보시길래 클립스튜디오 ex 기준으로 사진에서 배경용 선화 추출하기 가이드 만든 김에 여기다가두 올려봅니다.... 저두 이번에 첨 써보는거라 설명이 후져가꼬.... https://t.co/HuNv5avd5W 에서 더욱 자세한 설명을 보실 수 잇습니다 그럼이만.... https://t.co/6TvvV3kOXc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			154
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HuNv5avd5W",
+					"expanded_url": "https://tips.clip-studio.com/ko-kr/articles/1164",
+					"display_url": "tips.clip-studio.com/ko-kr/articles…",
+					"indices": [
+						98,
+						121
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1091283603474927600,
+					"id_str": "1091283603474927616",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/DyUEp85U0AAkSp2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyUEp85U0AAkSp2.jpg",
+					"url": "https://t.co/6TvvV3kOXc",
+					"display_url": "pic.twitter.com/6TvvV3kOXc",
+					"expanded_url": "https://twitter.com/mossddkk/status/1091283635452338176/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 435,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1100,
+							"h": 1719,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1091283603474927600,
+					"id_str": "1091283603474927616",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/DyUEp85U0AAkSp2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyUEp85U0AAkSp2.jpg",
+					"url": "https://t.co/6TvvV3kOXc",
+					"display_url": "pic.twitter.com/6TvvV3kOXc",
+					"expanded_url": "https://twitter.com/mossddkk/status/1091283635452338176/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 435,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1100,
+							"h": 1719,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1091283603466506200,
+					"id_str": "1091283603466506240",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/DyUEp83UUAAyMWk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyUEp83UUAAyMWk.jpg",
+					"url": "https://t.co/6TvvV3kOXc",
+					"display_url": "pic.twitter.com/6TvvV3kOXc",
+					"expanded_url": "https://twitter.com/mossddkk/status/1091283635452338176/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 435,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1100,
+							"h": 1719,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1091283603466534900,
+					"id_str": "1091283603466534912",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/DyUEp83UwAAsBLQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyUEp83UwAAsBLQ.jpg",
+					"url": "https://t.co/6TvvV3kOXc",
+					"display_url": "pic.twitter.com/6TvvV3kOXc",
+					"expanded_url": "https://twitter.com/mossddkk/status/1091283635452338176/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1219,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 714,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 405,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": 1090942968226144300,
+		"in_reply_to_status_id_str": "1090942968226144256",
+		"in_reply_to_user_id": 861627439226671100,
+		"in_reply_to_user_id_str": "861627439226671104",
+		"in_reply_to_screen_name": "mossddkk",
+		"user": {
+			"id": 861627439226671100,
+			"id_str": "861627439226671104",
+			"name": "💙노멀광인💙",
+			"screen_name": "mossddkk",
+			"location": "파리시 화예그룹빌딩",
+			"description": "드디어미쳐버린....여캐광인...... 1차창작/럽끄듀레벅....",
+			"url": "https://t.co/ONvveDxxE4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ONvveDxxE4",
+							"expanded_url": "https://marimo0960.postype.com",
+							"display_url": "marimo0960.postype.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4234,
+			"friends_count": 99,
+			"listed_count": 33,
+			"created_at": "Mon May 08 17:02:44 +0000 2017",
+			"favourites_count": 1354,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1600,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1031791800791400453/FVxS4HSa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1031791800791400453/FVxS4HSa_normal.jpg",
+			"profile_link_color": "0040FF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2791,
+		"favorite_count": 2145,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "지인분이 물어보시길래 클립스튜디오 ex 기준으로 사진에서 배경용 선화 추출하기 가이드 만든 김에 여기다가두 올려봅니다.... 저두 이번에 첨 써보는거라 설명이 후져가꼬.... https://t.co/HuNv5avd5W 에서 더욱 자세한 설명을 보실 수 잇습니다 그럼이만.... https://t.co/6TvvV3kOXc"
+	},
+	{
+		"created_at": "Tue Jan 29 19:02:36 +0000 2019",
+		"id": 1090324348198477800,
+		"id_str": "1092550887623483392",
+		"full_text": "제가 클튜 팁 올려둔거 모멘트로 모아둔 링크..\n\nhttps://t.co/GbRhhGr1Ty",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			51
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GbRhhGr1Ty",
+					"expanded_url": "https://twitter.com/i/moments/1067446797075800064",
+					"display_url": "twitter.com/i/moments/1067…",
+					"indices": [
+						28,
+						51
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1090319592688255000,
+		"in_reply_to_status_id_str": "1090319592688254976",
+		"in_reply_to_user_id": 947108730578931700,
+		"in_reply_to_user_id_str": "947108730578931713",
+		"in_reply_to_screen_name": "Baoryu",
+		"user": {
+			"id": 947108730578931700,
+			"id_str": "947108730578931713",
+			"name": "네사 뒤 바오류",
+			"screen_name": "Baoryu",
+			"location": "방학최고",
+			"description": "자캐본진 1차 손페티 파랑성애자 우주잡덕 섹트/야짤 그냥 올리는 편이니 주의해주세요! [FUB프리 헤어질땐 블언블] 앤캐 사랑해! 헤더 릴리 / 그림공부+커미션계 @Ryu_tale",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 494,
+			"friends_count": 381,
+			"listed_count": 4,
+			"created_at": "Sat Dec 30 14:14:52 +0000 2017",
+			"favourites_count": 3921,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 59929,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1089191759077240832/Rx8KVPW7_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1089191759077240832/Rx8KVPW7_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/947108730578931713/1549048870",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2256,
+		"favorite_count": 2128,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "제가 클튜 팁 올려둔거 모멘트로 모아둔 링크..\n\nhttps://t.co/GbRhhGr1Ty"
+	},
+	{
 		"created_at": "Wed Aug 23 10:34:52 +0000 2017",
 		"id": 900305330545897500,
 		"id_str": "1092144395011293184",
