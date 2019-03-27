3007cb3e4b59483d0ddc11e29c8719897d13e702
ggongkui <32505785+ggongkui@users.noreply.github.com>
2019-03-27 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8581,
-	"created_at": "Tue Mar 26 00:15:30 +0000 2019",
+	"tweets": 8582,
+	"created_at": "Wed Mar 27 00:15:29 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,176 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Tue Mar 26 09:59:20 +0000 2019",
+		"id": 1110143219801575400,
+		"id_str": "1110481350593642502",
+		"full_text": "손으로 그린 외치는? 말풍선 소재를 공유합니다. 제가 쓰려고 만든건데 마음에 드시는 분들 써주시면 좋겠어요\n파일을 여셔서 소재창에 레이어를 드래그하면 추가됩니다. \nhttps://t.co/Pj5eouuHS2 https://t.co/qkOHxqtxFP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			115
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Pj5eouuHS2",
+					"expanded_url": "https://drive.google.com/open?id=1V8mkCs_YNDB142xDVSMIRRxYZTWhPgod",
+					"display_url": "drive.google.com/open?id=1V8mkC…",
+					"indices": [
+						92,
+						115
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1110138844492423200,
+					"id_str": "1110138844492423168",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/D2gBZbVVYAAoOWu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2gBZbVVYAAoOWu.jpg",
+					"url": "https://t.co/qkOHxqtxFP",
+					"display_url": "pic.twitter.com/qkOHxqtxFP",
+					"expanded_url": "https://twitter.com/yakston/status/1110143219801575424/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1110138844492423200,
+					"id_str": "1110138844492423168",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/D2gBZbVVYAAoOWu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2gBZbVVYAAoOWu.jpg",
+					"url": "https://t.co/qkOHxqtxFP",
+					"display_url": "pic.twitter.com/qkOHxqtxFP",
+					"expanded_url": "https://twitter.com/yakston/status/1110143219801575424/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1110122466683179000,
+			"id_str": "1110122466683179008",
+			"name": "조약돌",
+			"screen_name": "yakston",
+			"location": "",
+			"description": "코미카-피냐타 작화  카카오페이지-크라이시스 작화\n현재 연재중인 작품에 관련된 내용을 주로 트윗합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18,
+			"friends_count": 2,
+			"listed_count": 0,
+			"created_at": "Mon Mar 25 10:13:15 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1110123000999763968/TPgHOL12_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1110123000999763968/TPgHOL12_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 530,
+		"favorite_count": 430,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "손으로 그린 외치는? 말풍선 소재를 공유합니다. 제가 쓰려고 만든건데 마음에 드시는 분들 써주시면 좋겠어요\n파일을 여셔서 소재창에 레이어를 드래그하면 추가됩니다. \nhttps://t.co/Pj5eouuHS2 https://t.co/qkOHxqtxFP"
+	},
+	{
 		"created_at": "Mon Mar 25 14:58:36 +0000 2019",
 		"id": 1110145316236349400,
 		"id_str": "1110194275562582016",
