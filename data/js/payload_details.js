fc9a2ddc92a95c805e10ce668ec11a69aa31b5ef
ggongkui <32505785+ggongkui@users.noreply.github.com>
2019-04-10 09:15:31 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8584,
-	"created_at": "Sat Mar 30 00:15:34 +0000 2019",
+	"tweets": 8585,
+	"created_at": "Wed Apr 10 00:15:30 +0000 2019",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2019_04.js
@@ -0,0 +1,96 @@
+Grailbird.data.tweets_2019_04 = 
+[
+	{
+		"created_at": "Tue Apr 09 09:13:47 +0000 2019",
+		"id": 1086860464275898400,
+		"id_str": "1115543319063420928",
+		"full_text": "https://t.co/TEjmJJTbA6\n\n와... 클튜 쓰시는 분들 이펜 졸라개좋음 어제 올라왔는데요 제가 써봤거든요? 진짜 완전 좋음 머박임 나는 앵간히 필압 적당히 없지만 잇고 얇지만 묘하게 안 얇은 클튜 펜 찾으시는 분은,... 완전 좋아요 러프하게 그릴 때도 좋음 홈쇼핑광고아님",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			161
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/TEjmJJTbA6",
+					"expanded_url": "https://assets.clip-studio.com/ja-jp/detail?id=1733530",
+					"display_url": "assets.clip-studio.com/ja-jp/detail?i…",
+					"indices": [
+						0,
+						23
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1081957509471002600,
+			"id_str": "1081957509471002624",
+			"name": "솔차",
+			"screen_name": "solcha_0927",
+			"location": "",
+			"description": "05 자캐 남곤 남자곤란줄임말",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 168,
+			"friends_count": 170,
+			"listed_count": 0,
+			"created_at": "Sun Jan 06 16:55:46 +0000 2019",
+			"favourites_count": 1675,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 8245,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1107297818337669121/iB2VXheH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1107297818337669121/iB2VXheH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1081957509471002624/1554296588",
+			"profile_link_color": "43FF22",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7132,
+		"favorite_count": 6748,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "https://t.co/TEjmJJTbA6\n\n와... 클튜 쓰시는 분들 이펜 졸라개좋음 어제 올라왔는데요 제가 써봤거든요? 진짜 완전 좋음 머박임 나는 앵간히 필압 적당히 없지만 잇고 얇지만 묘하게 안 얇은 클튜 펜 찾으시는 분은,... 완전 좋아요 러프하게 그릴 때도 좋음 홈쇼핑광고아님"
+	}
+]
