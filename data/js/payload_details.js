0fbf412981aeaab40f0d758a68483cab94a77f63
ggongkui <32505785+ggongkui@users.noreply.github.com>
2019-04-26 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8594,
-	"created_at": "Tue Apr 23 00:15:30 +0000 2019",
+	"tweets": 8595,
+	"created_at": "Fri Apr 26 00:15:30 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Thu Apr 25 05:01:28 +0000 2019",
+		"id": 1121266695841173500,
+		"id_str": "1121278024589041664",
+		"full_text": "Brie plays Beat Saber on @FallonTonight \nWatch here: https://t.co/8wZtUtF3i2 https://t.co/VkqVhFXCjQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			76
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "FallonTonight",
+					"name": "Fallon Tonight",
+					"id": 19777398,
+					"id_str": "19777398",
+					"indices": [
+						25,
+						39
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/8wZtUtF3i2",
+					"expanded_url": "https://youtu.be/05pzUXujMJU",
+					"display_url": "youtu.be/05pzUXujMJU",
+					"indices": [
+						53,
+						76
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1121266038925156400,
+					"id_str": "1121266038925156352",
+					"indices": [
+						77,
+						100
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1121266038925156352/pu/img/4bETmViIDIKz7ZKU.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1121266038925156352/pu/img/4bETmViIDIKz7ZKU.jpg",
+					"url": "https://t.co/VkqVhFXCjQ",
+					"display_url": "pic.twitter.com/VkqVhFXCjQ",
+					"expanded_url": "https://twitter.com/brielarsonhq/status/1121266695841173504/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1121266038925156400,
+					"id_str": "1121266038925156352",
+					"indices": [
+						77,
+						100
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1121266038925156352/pu/img/4bETmViIDIKz7ZKU.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1121266038925156352/pu/img/4bETmViIDIKz7ZKU.jpg",
+					"url": "https://t.co/VkqVhFXCjQ",
+					"display_url": "pic.twitter.com/VkqVhFXCjQ",
+					"expanded_url": "https://twitter.com/brielarsonhq/status/1121266695841173504/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 72906,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1121266038925156352/pu/vid/320x180/4prpr_-LMZK6anmw.mp4?tag=8"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1121266038925156352/pu/vid/640x360/hUaYOPlqXBDmxyRT.mp4?tag=8"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1121266038925156352/pu/vid/1280x720/5GTOtp2bYC-f4ION.mp4?tag=8"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1121266038925156352/pu/pl/gh4UNyqyM14cBV6i.m3u8?tag=8"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1081861430725541900,
+			"id_str": "1081861430725541889",
+			"name": "Brie Larson Updates",
+			"screen_name": "brielarsonhq",
+			"location": "follow our instagram",
+			"description": "Your source for daily updates on Brie Larson!",
+			"url": "https://t.co/sMpcHsxmG5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/sMpcHsxmG5",
+							"expanded_url": "https://www.instagram.com/brielarsonupdate/",
+							"display_url": "instagram.com/brielarsonupda…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5431,
+			"friends_count": 42,
+			"listed_count": 26,
+			"created_at": "Sun Jan 06 10:33:59 +0000 2019",
+			"favourites_count": 5670,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3300,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1119595391500996608/tJ-MMhaA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1119595391500996608/tJ-MMhaA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1081861430725541889/1555767316",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 872,
+		"favorite_count": 980,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Brie plays Beat Saber on @FallonTonight \nWatch here: https://t.co/8wZtUtF3i2 https://t.co/VkqVhFXCjQ"
+	},
+	{
 		"created_at": "Mon Apr 22 03:33:46 +0000 2019",
 		"id": 1119159029517836300,
 		"id_str": "1120168791999045632",
