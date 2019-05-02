c6aeee75999b9e7bbd4e6e06d53e921b909fb592
ggongkui <32505785+ggongkui@users.noreply.github.com>
2019-05-02 09:15:31 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8595,
-	"created_at": "Fri Apr 26 00:15:30 +0000 2019",
+	"tweets": 8596,
+	"created_at": "Thu May 02 00:15:29 +0000 2019",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2019_05.js
@@ -0,0 +1,233 @@
+Grailbird.data.tweets_2019_05 = 
+[
+	{
+		"created_at": "Wed May 01 04:58:58 +0000 2019",
+		"id": 1030037928825634800,
+		"id_str": "1123451724998553600",
+		"full_text": "&lt;초파리 퇴치제&gt; 이거 정말 간단하고 유익한 비법이다. 치약 뜨거운 물 살짝 넣어서 녹이고, 소주 5배 희석하면 끝. 분무기에 담아서 쓰레기통이나 싱크대 등에 뿌리면 초파리가 안꼬인다고. 초파리가 치약냄새를 엄청 싫어하는데 소주의 알콜이 그 향을 확산시켜줌. 쓰레기통 쓸때 한 번씩 뿌리면 끝. https://t.co/u535iCTsF9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			170
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1030037910060290000,
+					"id_str": "1030037910060290048",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/media/DktuBZZUUAAJrEJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DktuBZZUUAAJrEJ.jpg",
+					"url": "https://t.co/u535iCTsF9",
+					"display_url": "pic.twitter.com/u535iCTsF9",
+					"expanded_url": "https://twitter.com/pimangtomato/status/1030037928825634818/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1030037910060290000,
+					"id_str": "1030037910060290048",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/media/DktuBZZUUAAJrEJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DktuBZZUUAAJrEJ.jpg",
+					"url": "https://t.co/u535iCTsF9",
+					"display_url": "pic.twitter.com/u535iCTsF9",
+					"expanded_url": "https://twitter.com/pimangtomato/status/1030037928825634818/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1030037920856465400,
+					"id_str": "1030037920856465408",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/media/DktuCBnU4AAHIUm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DktuCBnU4AAHIUm.jpg",
+					"url": "https://t.co/u535iCTsF9",
+					"display_url": "pic.twitter.com/u535iCTsF9",
+					"expanded_url": "https://twitter.com/pimangtomato/status/1030037928825634818/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1030037270789681200,
+		"in_reply_to_status_id_str": "1030037270789681152",
+		"in_reply_to_user_id": 1371881846,
+		"in_reply_to_user_id_str": "1371881846",
+		"in_reply_to_screen_name": "pimangtomato",
+		"user": {
+			"id": 1371881846,
+			"id_str": "1371881846",
+			"name": "신촌 피망과토마토 망가BAR AGAIN!",
+			"screen_name": "pimangtomato",
+			"location": "서대문구 창천동 명물길 27-21, 2층 (46-3)",
+			"description": "KOREAN의 자랑이 되겠습니다.\n\n저녁5시~새벽2시\n\n맥주, 음악, 만화",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3081,
+			"friends_count": 603,
+			"listed_count": 33,
+			"created_at": "Mon Apr 22 11:13:36 +0000 2013",
+			"favourites_count": 13305,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 24674,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1112784228603686915/LxcGqoSF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1112784228603686915/LxcGqoSF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1371881846/1549993491",
+			"profile_link_color": "FF691F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "01a241300ef51ff0",
+			"url": "https://api.twitter.com/1.1/geo/id/01a241300ef51ff0.json",
+			"place_type": "city",
+			"name": "서대문구",
+			"full_name": "대한민국 서대문구",
+			"country_code": "KR",
+			"country": "대한민국",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							126.909647851736,
+							37.5525984433707
+						],
+						[
+							126.970218583433,
+							37.5525984433707
+						],
+						[
+							126.970218583433,
+							37.600879441397
+						],
+						[
+							126.909647851736,
+							37.600879441397
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5792,
+		"favorite_count": 3064,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "&lt;초파리 퇴치제&gt; 이거 정말 간단하고 유익한 비법이다. 치약 뜨거운 물 살짝 넣어서 녹이고, 소주 5배 희석하면 끝. 분무기에 담아서 쓰레기통이나 싱크대 등에 뿌리면 초파리가 안꼬인다고. 초파리가 치약냄새를 엄청 싫어하는데 소주의 알콜이 그 향을 확산시켜줌. 쓰레기통 쓸때 한 번씩 뿌리면 끝. https://t.co/u535iCTsF9"
+	}
+]
