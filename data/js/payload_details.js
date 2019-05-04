aa93641a2ef1469d5fe169bd860230c8795da56c
ggongkui <32505785+ggongkui@users.noreply.github.com>
2019-05-04 09:15:31 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8596,
-	"created_at": "Thu May 02 00:15:29 +0000 2019",
+	"tweets": 8597,
+	"created_at": "Sat May 04 00:15:29 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Fri May 03 05:03:07 +0000 2019",
+		"id": 1123565983992467500,
+		"id_str": "1124177544658341888",
+		"full_text": "클래식 추천 https://t.co/78MdfuJwiA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			6
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1123565975347949600,
+					"id_str": "1123565975347949570",
+					"indices": [
+						7,
+						30
+					],
+					"media_url": "http://pbs.twimg.com/media/D5e1TTpUcAIAYE5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5e1TTpUcAIAYE5.jpg",
+					"url": "https://t.co/78MdfuJwiA",
+					"display_url": "pic.twitter.com/78MdfuJwiA",
+					"expanded_url": "https://twitter.com/wildcatclub_/status/1123565983992467457/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1082,
+							"h": 976,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 613,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1082,
+							"h": 976,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1123565975347949600,
+					"id_str": "1123565975347949570",
+					"indices": [
+						7,
+						30
+					],
+					"media_url": "http://pbs.twimg.com/media/D5e1TTpUcAIAYE5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5e1TTpUcAIAYE5.jpg",
+					"url": "https://t.co/78MdfuJwiA",
+					"display_url": "pic.twitter.com/78MdfuJwiA",
+					"expanded_url": "https://twitter.com/wildcatclub_/status/1123565983992467457/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1082,
+							"h": 976,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 613,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1082,
+							"h": 976,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1123565975339556900,
+					"id_str": "1123565975339556864",
+					"indices": [
+						7,
+						30
+					],
+					"media_url": "http://pbs.twimg.com/media/D5e1TTnUYAAwboU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5e1TTnUYAAwboU.jpg",
+					"url": "https://t.co/78MdfuJwiA",
+					"display_url": "pic.twitter.com/78MdfuJwiA",
+					"expanded_url": "https://twitter.com/wildcatclub_/status/1123565983992467457/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 861,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 2008,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 488,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4621221529,
+			"id_str": "4621221529",
+			"name": "유용한 정보 저장/백업계",
+			"screen_name": "wildcatclub_",
+			"location": "",
+			"description": "유용한 팁을 저장, 백업합니다",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 68018,
+			"friends_count": 0,
+			"listed_count": 243,
+			"created_at": "Mon Dec 21 11:16:05 +0000 2015",
+			"favourites_count": 16,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 308,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/938454495712784384/5sCqoZ_t_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/938454495712784384/5sCqoZ_t_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 21801,
+		"favorite_count": 18580,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "클래식 추천 https://t.co/78MdfuJwiA"
+	},
+	{
 		"created_at": "Wed May 01 04:58:58 +0000 2019",
 		"id": 1030037928825634800,
 		"id_str": "1123451724998553600",
