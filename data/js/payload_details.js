34caf5829168d8579ce97e8c7c34735fae3e9fae
ggongkui <32505785+ggongkui@users.noreply.github.com>
2019-06-02 09:15:31 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8609,
-	"created_at": "Sat Jun 01 00:15:30 +0000 2019",
+	"tweets": 8611,
+	"created_at": "Sun Jun 02 00:15:30 +0000 2019",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2019_06.js
@@ -0,0 +1,420 @@
+Grailbird.data.tweets_2019_06 = 
+[
+	{
+		"created_at": "Sat Jun 01 14:12:52 +0000 2019",
+		"id": 1118172317152976900,
+		"id_str": "1134825140204724225",
+		"full_text": "5. 뭘 소개할려니까 의외로 클튜 소재를 빡시게 쓴 그림이 업네유\n하지만 이분 패턴은 소개하고 가야겠음 완전 간쥐나느 패턴장인이신데 이분이 올리신 패턴 전체가 다 예쁘고 조읍니다 ^^  그림은 아주 미묘하게 패턴 섞은거\n\nhttps://t.co/5s0Y3qNMHg &lt;무 . 료 https://t.co/Gb8eFtt5zk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			157
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5s0Y3qNMHg",
+					"expanded_url": "https://assets.clip-studio.com/ja-jp/detail?id=1737857",
+					"display_url": "assets.clip-studio.com/ja-jp/detail?i…",
+					"indices": [
+						124,
+						147
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1118172246063759400,
+					"id_str": "1118172246063759367",
+					"indices": [
+						158,
+						181
+					],
+					"media_url": "http://pbs.twimg.com/media/D4SLu8RUwAcqZ9s.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D4SLu8RUwAcqZ9s.jpg",
+					"url": "https://t.co/Gb8eFtt5zk",
+					"display_url": "pic.twitter.com/Gb8eFtt5zk",
+					"expanded_url": "https://twitter.com/Onestep_FW/status/1118172317152976897/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 984,
+							"h": 707,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 984,
+							"h": 707,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 489,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1118172246063759400,
+					"id_str": "1118172246063759367",
+					"indices": [
+						158,
+						181
+					],
+					"media_url": "http://pbs.twimg.com/media/D4SLu8RUwAcqZ9s.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D4SLu8RUwAcqZ9s.jpg",
+					"url": "https://t.co/Gb8eFtt5zk",
+					"display_url": "pic.twitter.com/Gb8eFtt5zk",
+					"expanded_url": "https://twitter.com/Onestep_FW/status/1118172317152976897/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 984,
+							"h": 707,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 984,
+							"h": 707,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 489,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1118172262820012000,
+					"id_str": "1118172262820012033",
+					"indices": [
+						158,
+						181
+					],
+					"media_url": "http://pbs.twimg.com/media/D4SLv6sU4AEq8TR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D4SLv6sU4AEq8TR.jpg",
+					"url": "https://t.co/Gb8eFtt5zk",
+					"display_url": "pic.twitter.com/Gb8eFtt5zk",
+					"expanded_url": "https://twitter.com/Onestep_FW/status/1118172317152976897/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1198,
+							"h": 282,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1198,
+							"h": 282,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 160,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1118172270688530400,
+					"id_str": "1118172270688530432",
+					"indices": [
+						158,
+						181
+					],
+					"media_url": "http://pbs.twimg.com/media/D4SLwYAU8AA92IP.png",
+					"media_url_https": "https://pbs.twimg.com/media/D4SLwYAU8AA92IP.png",
+					"url": "https://t.co/Gb8eFtt5zk",
+					"display_url": "pic.twitter.com/Gb8eFtt5zk",
+					"expanded_url": "https://twitter.com/Onestep_FW/status/1118172317152976897/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 348,
+							"h": 410,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 348,
+							"h": 410,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 348,
+							"h": 410,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1118171983634554900,
+		"in_reply_to_status_id_str": "1118171983634554883",
+		"in_reply_to_user_id": 899262421331697700,
+		"in_reply_to_user_id_str": "899262421331697665",
+		"in_reply_to_screen_name": "Onestep_FW",
+		"user": {
+			"id": 899262421331697700,
+			"id_str": "899262421331697665",
+			"name": "챠╰(*°▽°*)╯리",
+			"screen_name": "Onestep_FW",
+			"location": "비밀의 화원",
+			"description": "여러가지 그리는 잡동사니 취향♪♩♩ / 주로 1차, 2차그림을 그립니다. ※완전 잡덕이니 주의/ FUB*free / 작업 관련 메일: carbonic@naver.com / Don't use my artwork without permission!! = 그림 무단 사용 금지🚫",
+			"url": "https://t.co/6WPZGaCTuv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6WPZGaCTuv",
+							"expanded_url": "https://www.pixiv.net/member.php?id=2047922",
+							"display_url": "pixiv.net/member.php?id=…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 719,
+			"friends_count": 29,
+			"listed_count": 14,
+			"created_at": "Sun Aug 20 13:30:43 +0000 2017",
+			"favourites_count": 28,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 191,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1128412574502899712/puh6Cady_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1128412574502899712/puh6Cady_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/899262421331697665/1557869585",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3142,
+		"favorite_count": 3079,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "5. 뭘 소개할려니까 의외로 클튜 소재를 빡시게 쓴 그림이 업네유\n하지만 이분 패턴은 소개하고 가야겠음 완전 간쥐나느 패턴장인이신데 이분이 올리신 패턴 전체가 다 예쁘고 조읍니다 ^^  그림은 아주 미묘하게 패턴 섞은거\n\nhttps://t.co/5s0Y3qNMHg &lt;무 . 료 https://t.co/Gb8eFtt5zk"
+	},
+	{
+		"created_at": "Sat Jun 01 10:53:13 +0000 2019",
+		"id": 1082645139246460900,
+		"id_str": "1134774898054688769",
+		"full_text": "클튜 백업 하는 법 https://t.co/F3Y8cGPGDY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			10
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1082645065447718900,
+					"id_str": "1082645065447718913",
+					"indices": [
+						11,
+						34
+					],
+					"media_url": "http://pbs.twimg.com/media/DwZT82_XQAEYBDw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DwZT82_XQAEYBDw.jpg",
+					"url": "https://t.co/F3Y8cGPGDY",
+					"display_url": "pic.twitter.com/F3Y8cGPGDY",
+					"expanded_url": "https://twitter.com/Clip_use222/status/1082645139246460928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 742,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 742,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 420,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1082645065447718900,
+					"id_str": "1082645065447718913",
+					"indices": [
+						11,
+						34
+					],
+					"media_url": "http://pbs.twimg.com/media/DwZT82_XQAEYBDw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DwZT82_XQAEYBDw.jpg",
+					"url": "https://t.co/F3Y8cGPGDY",
+					"display_url": "pic.twitter.com/F3Y8cGPGDY",
+					"expanded_url": "https://twitter.com/Clip_use222/status/1082645139246460928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 742,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 742,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 420,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1068737139523735600,
+			"id_str": "1068737139523735553",
+			"name": "갓클",
+			"screen_name": "Clip_use222",
+			"location": "",
+			"description": "클튜 기초 기능 팁 = 메인트 타래  / 아는 것만 알려드립니다.....업로드 천천히함...... + 설명 보실땐 웬만하면 타래 끝까지 읽어주세요!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5140,
+			"friends_count": 1,
+			"listed_count": 37,
+			"created_at": "Sat Dec 01 05:22:45 +0000 2018",
+			"favourites_count": 14,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 246,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1068738828154748928/j2HvBTCZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1068738828154748928/j2HvBTCZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1068737139523735553/1546104816",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 702,
+		"favorite_count": 525,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "클튜 백업 하는 법 https://t.co/F3Y8cGPGDY"
+	}
+]
