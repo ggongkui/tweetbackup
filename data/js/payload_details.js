b16dbec37d09aa98a8e61b409297120f908ddc93
ggongkui <32505785+ggongkui@users.noreply.github.com>
2019-08-05 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8641,
-	"created_at": "Wed Jul 31 00:15:29 +0000 2019",
+	"tweets": 8642,
+	"created_at": "Mon Aug 05 00:15:29 +0000 2019",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2019_08.js
@@ -0,0 +1,109 @@
+Grailbird.data.tweets_2019_08 = 
+[
+	{
+		"created_at": "Sun Aug 04 05:22:52 +0000 2019",
+		"id": 1157628959938322400,
+		"id_str": "1157884584391929856",
+		"full_text": "https://t.co/siWlu9Vya5 이 브러쉬 진자 너무 좋다 없이는 원고 못해",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			48
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/siWlu9Vya5",
+					"expanded_url": "https://assets.clip-studio.com/ja-jp/detail?id=1724191",
+					"display_url": "assets.clip-studio.com/ja-jp/detail?i…",
+					"indices": [
+						0,
+						23
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2370718164,
+			"id_str": "2370718164",
+			"name": "(모온日/B5)탄섬",
+			"screen_name": "Tansum0_0",
+			"location": "I love you, three thousand.",
+			"description": "20↑ 유중혁🥟과 성현제🧠에 진심인 사람 | Kor Eng 연락은 tansum@naver.com I 콘부님 H 다거님 / 비팔알림off",
+			"url": "https://t.co/t5LKTcu1Mo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t5LKTcu1Mo",
+							"expanded_url": "https://peing.net/ko/tansum0_0",
+							"display_url": "peing.net/ko/tansum0_0",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9183,
+			"friends_count": 360,
+			"listed_count": 78,
+			"created_at": "Mon Mar 03 17:29:04 +0000 2014",
+			"favourites_count": 16022,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6720,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1156205166598619136/FmMygz-d_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1156205166598619136/FmMygz-d_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2370718164/1562174621",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1721,
+		"favorite_count": 1876,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "https://t.co/siWlu9Vya5 이 브러쉬 진자 너무 좋다 없이는 원고 못해"
+	}
+]
