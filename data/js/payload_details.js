2c5ad2871241b11baa86d2af626c1ddce0e8e4cb
ggongkui <32505785+ggongkui@users.noreply.github.com>
2019-11-05 09:15:31 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8678,
-	"created_at": "Fri Nov 01 00:15:30 +0000 2019",
+	"tweets": 8680,
+	"created_at": "Tue Nov 05 00:15:30 +0000 2019",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2019_11.js
@@ -0,0 +1,822 @@
+Grailbird.data.tweets_2019_11 = 
+[
+	{
+		"created_at": "Mon Nov 04 17:50:00 +0000 2019",
+		"id": 1157608878684561400,
+		"id_str": "1191412293227053056",
+		"full_text": "커미션틀 이었는데 안쓸것 같아서 배포합니다~ 수정,변형,출처표기 자유 재배포만 삼가해주세요😘 #트레이스 #트레틀 https://t.co/EYRBPADm9M",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			62
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "트레이스",
+					"indices": [
+						52,
+						57
+					]
+				},
+				{
+					"text": "트레틀",
+					"indices": [
+						58,
+						62
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1157608851132125200,
+					"id_str": "1157608851132125185",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/EBCnHeiUEAEtIm6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBCnHeiUEAEtIm6.jpg",
+					"url": "https://t.co/EYRBPADm9M",
+					"display_url": "pic.twitter.com/EYRBPADm9M",
+					"expanded_url": "https://twitter.com/PY0R0R0/status/1157608878684561416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1109,
+							"h": 911,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1109,
+							"h": 911,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 559,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 530,
+									"y": 236,
+									"h": 41,
+									"w": 41
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 530,
+									"y": 236,
+									"h": 41,
+									"w": 41
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 530,
+									"y": 236,
+									"h": 41,
+									"w": 41
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 324,
+									"y": 144,
+									"h": 25,
+									"w": 25
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1157608851132125200,
+					"id_str": "1157608851132125185",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/EBCnHeiUEAEtIm6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBCnHeiUEAEtIm6.jpg",
+					"url": "https://t.co/EYRBPADm9M",
+					"display_url": "pic.twitter.com/EYRBPADm9M",
+					"expanded_url": "https://twitter.com/PY0R0R0/status/1157608878684561416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1109,
+							"h": 911,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1109,
+							"h": 911,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 559,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 530,
+									"y": 236,
+									"h": 41,
+									"w": 41
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 530,
+									"y": 236,
+									"h": 41,
+									"w": 41
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 530,
+									"y": 236,
+									"h": 41,
+									"w": 41
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 324,
+									"y": 144,
+									"h": 25,
+									"w": 25
+								}
+							]
+						}
+					}
+				},
+				{
+					"id": 1157608851132174300,
+					"id_str": "1157608851132174336",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/EBCnHeiU0AAGP9s.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBCnHeiU0AAGP9s.jpg",
+					"url": "https://t.co/EYRBPADm9M",
+					"display_url": "pic.twitter.com/EYRBPADm9M",
+					"expanded_url": "https://twitter.com/PY0R0R0/status/1157608878684561416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 556,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1112,
+							"h": 910,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1112,
+							"h": 910,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3298023348,
+			"id_str": "3298023348",
+			"name": "뾰로",
+			"screen_name": "PY0R0R0",
+			"location": "ⓘ먼지잼님 ⓗ상자님 커미션",
+			"description": "ピョロ pyopyo46@naver.com",
+			"url": "https://t.co/wlssluALJx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wlssluALJx",
+							"expanded_url": "http://pyorocommission.tistory.com/notice",
+							"display_url": "pyorocommission.tistory.com/notice",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 64,
+			"friends_count": 19,
+			"listed_count": 1,
+			"created_at": "Mon Jul 27 16:17:14 +0000 2015",
+			"favourites_count": 39,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 203,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1160946365288996865/VELwLM6n_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1160946365288996865/VELwLM6n_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3298023348/1544170328",
+			"profile_link_color": "F5ABB5",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 710,
+		"favorite_count": 499,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "커미션틀 이었는데 안쓸것 같아서 배포합니다~ 수정,변형,출처표기 자유 재배포만 삼가해주세요😘 #트레이스 #트레틀 https://t.co/EYRBPADm9M"
+	},
+	{
+		"created_at": "Mon Nov 04 17:46:34 +0000 2019",
+		"id": 1152872608884379600,
+		"id_str": "1191411426138546177",
+		"full_text": "8가지 러프틀(1인/여름x밀집모자x해바라기) 배포합니다. 즐겁게 연성에 사용하여주세요. 🙆‍♀️\n🚩타커미션 신청 사용 가능 / 굿즈 제작 허용/ 프리 변형 허용/프사 헤더 편집 사용가능합니다. /자유사용 / 재배포는 되도록 삼가하여주세요 :) https://t.co/1QczQ0MXAX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			135
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1152871749672820700,
+					"id_str": "1152871749672820736",
+					"indices": [
+						136,
+						159
+					],
+					"media_url": "http://pbs.twimg.com/media/D__Sv4bU4AAwBh3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D__Sv4bU4AAwBh3.jpg",
+					"url": "https://t.co/1QczQ0MXAX",
+					"display_url": "pic.twitter.com/1QczQ0MXAX",
+					"expanded_url": "https://twitter.com/damso_2018/status/1152872608884379648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 492,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 868,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1403,
+							"h": 1015,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 514,
+									"y": 119,
+									"h": 74,
+									"w": 74
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 907,
+									"y": 211,
+									"h": 131,
+									"w": 131
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 1061,
+									"y": 247,
+									"h": 154,
+									"w": 154
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 1061,
+									"y": 247,
+									"h": 154,
+									"w": 154
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1152871749672820700,
+					"id_str": "1152871749672820736",
+					"indices": [
+						136,
+						159
+					],
+					"media_url": "http://pbs.twimg.com/media/D__Sv4bU4AAwBh3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D__Sv4bU4AAwBh3.jpg",
+					"url": "https://t.co/1QczQ0MXAX",
+					"display_url": "pic.twitter.com/1QczQ0MXAX",
+					"expanded_url": "https://twitter.com/damso_2018/status/1152872608884379648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 492,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 868,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1403,
+							"h": 1015,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 514,
+									"y": 119,
+									"h": 74,
+									"w": 74
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 907,
+									"y": 211,
+									"h": 131,
+									"w": 131
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 1061,
+									"y": 247,
+									"h": 154,
+									"w": 154
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 1061,
+									"y": 247,
+									"h": 154,
+									"w": 154
+								}
+							]
+						}
+					}
+				},
+				{
+					"id": 1152871819465986000,
+					"id_str": "1152871819465986048",
+					"indices": [
+						136,
+						159
+					],
+					"media_url": "http://pbs.twimg.com/media/D__Sz8bUEAAF1h7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D__Sz8bUEAAF1h7.jpg",
+					"url": "https://t.co/1QczQ0MXAX",
+					"display_url": "pic.twitter.com/1QczQ0MXAX",
+					"expanded_url": "https://twitter.com/damso_2018/status/1152872608884379648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 492,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1403,
+							"h": 1015,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 868,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 291,
+									"y": 224,
+									"h": 58,
+									"w": 58
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 602,
+									"y": 463,
+									"h": 120,
+									"w": 120
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 602,
+									"y": 463,
+									"h": 120,
+									"w": 120
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 514,
+									"y": 396,
+									"h": 102,
+									"w": 102
+								}
+							]
+						}
+					}
+				},
+				{
+					"id": 1152871982725120000,
+					"id_str": "1152871982725120000",
+					"indices": [
+						136,
+						159
+					],
+					"media_url": "http://pbs.twimg.com/media/D__S9cnUwAADzp0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D__S9cnUwAADzp0.jpg",
+					"url": "https://t.co/1QczQ0MXAX",
+					"display_url": "pic.twitter.com/1QczQ0MXAX",
+					"expanded_url": "https://twitter.com/damso_2018/status/1152872608884379648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1403,
+							"h": 1015,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 868,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 492,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": [
+								{
+									"x": 1185,
+									"y": 406,
+									"h": 67,
+									"w": 67
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 1185,
+									"y": 406,
+									"h": 67,
+									"w": 67
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 1013,
+									"y": 347,
+									"h": 57,
+									"w": 57
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 574,
+									"y": 196,
+									"h": 32,
+									"w": 32
+								}
+							]
+						}
+					}
+				},
+				{
+					"id": 1152872054917439500,
+					"id_str": "1152872054917439489",
+					"indices": [
+						136,
+						159
+					],
+					"media_url": "http://pbs.twimg.com/media/D__TBpjUIAEsWFX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D__TBpjUIAEsWFX.jpg",
+					"url": "https://t.co/1QczQ0MXAX",
+					"display_url": "pic.twitter.com/1QczQ0MXAX",
+					"expanded_url": "https://twitter.com/damso_2018/status/1152872608884379648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1403,
+							"h": 1015,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 492,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 868,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 198,
+									"y": 397,
+									"h": 80,
+									"w": 80
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 198,
+									"y": 397,
+									"h": 80,
+									"w": 80
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 95,
+									"y": 192,
+									"h": 38,
+									"w": 38
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 169,
+									"y": 339,
+									"h": 68,
+									"w": 68
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 828502440727109600,
+			"id_str": "828502440727109633",
+			"name": "🍓담소/슬롯❌",
+			"screen_name": "damso_2018",
+			"location": "",
+			"description": "바쁜 오프라인으로 유동적으로 슬롯이 닫히고 열립니다. /커미션에 관한 저작권에 대해서는 커미션 홈페이지 주의사항 한번씩 확인해주세요./배포모음은 팔로잉계정 확인/인장 체카님 커미션(@cherryalpaka)헤더 꽁님 커미션(@mprhddnjs )",
+			"url": "https://t.co/truLq9odJT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/truLq9odJT",
+							"expanded_url": "https://ppoxx.tistory.com/5",
+							"display_url": "ppoxx.tistory.com/5",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11287,
+			"friends_count": 1,
+			"listed_count": 139,
+			"created_at": "Mon Feb 06 07:15:49 +0000 2017",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 913,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1175716844885307392/lGvtMrj-_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1175716844885307392/lGvtMrj-_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/828502440727109633/1569148094",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14179,
+		"favorite_count": 12018,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "8가지 러프틀(1인/여름x밀집모자x해바라기) 배포합니다. 즐겁게 연성에 사용하여주세요. 🙆‍♀️\n🚩타커미션 신청 사용 가능 / 굿즈 제작 허용/ 프리 변형 허용/프사 헤더 편집 사용가능합니다. /자유사용 / 재배포는 되도록 삼가하여주세요 :) https://t.co/1QczQ0MXAX"
+	}
+]
