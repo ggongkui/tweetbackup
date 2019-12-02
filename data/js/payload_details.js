0256c440d2f097cc8b157264f41c5978c8ebfff4
ggongkui <32505785+ggongkui@users.noreply.github.com>
2019-12-02 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8689,
-	"created_at": "Sat Nov 23 00:15:29 +0000 2019",
+	"tweets": 8690,
+	"created_at": "Mon Dec 02 00:15:29 +0000 2019",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,345 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Sun Dec 01 17:21:49 +0000 2019",
+		"id": 972318814489727000,
+		"id_str": "1201189670039670784",
+		"full_text": "🌸사랑에 빠지는 36가지의 질문🌸 이라고 미국에서 되게 유명한데 한국어로도 있네요!\n\n연인들과 해보시길 추천해드려요!\n진짜 새삼 다시돌아보고 생각하게 되고,\n서로 오래사귀었어도 몰랐던 점들이 계속 나와요!\n지금 와리님이랑 16번까지 했는데 몇시간 걸림ㅋㅋㅋㅋㅋㅋㅋㅋㅋ https://t.co/TxqpQegx3e",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			149
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 972318808391086100,
+					"id_str": "972318808391086080",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/DX5ezf6U8AA393-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DX5ezf6U8AA393-.jpg",
+					"url": "https://t.co/TxqpQegx3e",
+					"display_url": "pic.twitter.com/TxqpQegx3e",
+					"expanded_url": "https://twitter.com/koohy91/status/972318814489726976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 665,
+							"h": 450,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 665,
+							"h": 450,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 665,
+							"h": 450,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 972318808391086100,
+					"id_str": "972318808391086080",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/DX5ezf6U8AA393-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DX5ezf6U8AA393-.jpg",
+					"url": "https://t.co/TxqpQegx3e",
+					"display_url": "pic.twitter.com/TxqpQegx3e",
+					"expanded_url": "https://twitter.com/koohy91/status/972318814489726976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 665,
+							"h": 450,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 665,
+							"h": 450,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 665,
+							"h": 450,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				},
+				{
+					"id": 972318808386842600,
+					"id_str": "972318808386842624",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/DX5ezf5UMAA_O5w.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DX5ezf5UMAA_O5w.jpg",
+					"url": "https://t.co/TxqpQegx3e",
+					"display_url": "pic.twitter.com/TxqpQegx3e",
+					"expanded_url": "https://twitter.com/koohy91/status/972318814489726976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 665,
+							"h": 450,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 665,
+							"h": 450,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 665,
+							"h": 450,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 69,
+									"y": 339,
+									"h": 62,
+									"w": 62
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 69,
+									"y": 339,
+									"h": 62,
+									"w": 62
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 69,
+									"y": 339,
+									"h": 62,
+									"w": 62
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 69,
+									"y": 339,
+									"h": 62,
+									"w": 62
+								}
+							]
+						}
+					}
+				},
+				{
+					"id": 972318808403624000,
+					"id_str": "972318808403623936",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/DX5ezf9UQAAlSEy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DX5ezf9UQAAlSEy.jpg",
+					"url": "https://t.co/TxqpQegx3e",
+					"display_url": "pic.twitter.com/TxqpQegx3e",
+					"expanded_url": "https://twitter.com/koohy91/status/972318814489726976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 665,
+							"h": 450,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 665,
+							"h": 450,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 665,
+							"h": 450,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 433921040,
+			"id_str": "433921040",
+			"name": "𝑳𝒆𝒆𝒀𝒆𝒐𝒏 리연🌸 Into the Unknown",
+			"screen_name": "koohy91",
+			"location": "Texas, USA i. 에스더님 & h. 동아님",
+			"description": "LeeYeon♬ リヨン♡ [성악가/음악교육학/지휘전공] #리연의화음가이드|체인블락가끔|★Nico:: mylist/9604093 💖와리연커플계정:: @LYRiCouple ★https://t.co/DlPWMHjIyJ 집사님《@Butlerfor_LY 》 ❤️@relationlife",
+			"url": "https://t.co/iKnfwhchF9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/iKnfwhchF9",
+							"expanded_url": "http://koohy91.wix.com/lycmusic",
+							"display_url": "koohy91.wix.com/lycmusic",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/DlPWMHjIyJ",
+							"expanded_url": "http://youtube.com/KuLeeYeon",
+							"display_url": "youtube.com/KuLeeYeon",
+							"indices": [
+								95,
+								118
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 788,
+			"friends_count": 306,
+			"listed_count": 9,
+			"created_at": "Sun Dec 11 05:52:32 +0000 2011",
+			"favourites_count": 12739,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11046,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FF6699",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme11/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme11/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1194059648824946689/HDmZPpmJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1194059648824946689/HDmZPpmJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/433921040/1562130103",
+			"profile_link_color": "B40B43",
+			"profile_sidebar_border_color": "CC3366",
+			"profile_sidebar_fill_color": "E5507E",
+			"profile_text_color": "362720",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8161,
+		"favorite_count": 4821,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "🌸사랑에 빠지는 36가지의 질문🌸 이라고 미국에서 되게 유명한데 한국어로도 있네요!\n\n연인들과 해보시길 추천해드려요!\n진짜 새삼 다시돌아보고 생각하게 되고,\n서로 오래사귀었어도 몰랐던 점들이 계속 나와요!\n지금 와리님이랑 16번까지 했는데 몇시간 걸림ㅋㅋㅋㅋㅋㅋㅋㅋㅋ https://t.co/TxqpQegx3e"
+	}
+]
