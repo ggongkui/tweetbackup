c31faf8a2110ed3b2a81b7062493db899f96590a
ggongkui <32505785+ggongkui@users.noreply.github.com>
2020-01-03 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8698,
-	"created_at": "Thu Jan 02 00:15:30 +0000 2020",
+	"tweets": 8699,
+	"created_at": "Fri Jan 03 00:15:29 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,403 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Thu Jan 02 05:21:34 +0000 2020",
+		"id": 1101367035315945500,
+		"id_str": "1212604826313322496",
+		"full_text": "사진 작가 Zack Seckler가 경비행기를 타고 상공에서 촬영한 남아프리카 사진 자연의 고요함에 압도당하는 기분인 https://t.co/eTfJjbQkLw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			65
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1101367003183370200,
+					"id_str": "1101367003183370245",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/media/D0jXdEAU0AUXF41.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0jXdEAU0AUXF41.jpg",
+					"url": "https://t.co/eTfJjbQkLw",
+					"display_url": "pic.twitter.com/eTfJjbQkLw",
+					"expanded_url": "https://twitter.com/1965_1122/status/1101367035315945472/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1101367003183370200,
+					"id_str": "1101367003183370245",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/media/D0jXdEAU0AUXF41.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0jXdEAU0AUXF41.jpg",
+					"url": "https://t.co/eTfJjbQkLw",
+					"display_url": "pic.twitter.com/eTfJjbQkLw",
+					"expanded_url": "https://twitter.com/1965_1122/status/1101367035315945472/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				},
+				{
+					"id": 1101367010733121500,
+					"id_str": "1101367010733121536",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/media/D0jXdgIU4AAQLGr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0jXdgIU4AAQLGr.jpg",
+					"url": "https://t.co/eTfJjbQkLw",
+					"display_url": "pic.twitter.com/eTfJjbQkLw",
+					"expanded_url": "https://twitter.com/1965_1122/status/1101367035315945472/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				},
+				{
+					"id": 1101367018626854900,
+					"id_str": "1101367018626854912",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/media/D0jXd9iVsAAThLX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0jXd9iVsAAThLX.jpg",
+					"url": "https://t.co/eTfJjbQkLw",
+					"display_url": "pic.twitter.com/eTfJjbQkLw",
+					"expanded_url": "https://twitter.com/1965_1122/status/1101367035315945472/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 900,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 600,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 470,
+									"y": 225,
+									"h": 89,
+									"w": 89
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 623,
+									"y": 298,
+									"h": 118,
+									"w": 118
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 623,
+									"y": 298,
+									"h": 118,
+									"w": 118
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 623,
+									"y": 298,
+									"h": 118,
+									"w": 118
+								}
+							]
+						}
+					}
+				},
+				{
+					"id": 1101367027350921200,
+					"id_str": "1101367027350921216",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/media/D0jXeeCUYAA1B7C.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0jXeeCUYAA1B7C.jpg",
+					"url": "https://t.co/eTfJjbQkLw",
+					"display_url": "pic.twitter.com/eTfJjbQkLw",
+					"expanded_url": "https://twitter.com/1965_1122/status/1101367035315945472/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 900,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 600,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 213,
+									"y": 113,
+									"h": 36,
+									"w": 36
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 282,
+									"y": 150,
+									"h": 48,
+									"w": 48
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 282,
+									"y": 150,
+									"h": 48,
+									"w": 48
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 282,
+									"y": 150,
+									"h": 48,
+									"w": 48
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 981514130333974500,
+			"id_str": "981514130333974528",
+			"name": "O",
+			"screen_name": "506_605",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8008,
+			"friends_count": 36,
+			"listed_count": 53,
+			"created_at": "Wed Apr 04 12:49:39 +0000 2018",
+			"favourites_count": 637,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 852,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1079770749513027584/aeMa_gV4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1079770749513027584/aeMa_gV4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/981514130333974528/1536227884",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7090,
+		"favorite_count": 7008,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "사진 작가 Zack Seckler가 경비행기를 타고 상공에서 촬영한 남아프리카 사진 자연의 고요함에 압도당하는 기분인 https://t.co/eTfJjbQkLw"
+	},
+	{
 		"created_at": "Wed Jan 01 12:58:40 +0000 2020",
 		"id": 1212246631485100000,
 		"id_str": "1212357471471165440",
