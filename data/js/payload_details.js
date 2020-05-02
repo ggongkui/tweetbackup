3380d1f6234c4d9405f084eb0099fea4e90bc61e
ggongkui <32505785+ggongkui@users.noreply.github.com>
2020-05-02 09:15:31 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8754,
-	"created_at": "Thu Apr 30 00:15:30 +0000 2020",
+	"tweets": 8755,
+	"created_at": "Sat May 02 00:15:30 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_05.js
@@ -0,0 +1,320 @@
+Grailbird.data.tweets_2020_05 = 
+[
+	{
+		"created_at": "Fri May 01 13:54:35 +0000 2020",
+		"id": 1256190830043295700,
+		"id_str": "1256220475996454914",
+		"full_text": "&lt;수트 드로잉 팁&gt;\n수트 그릴 때 핏 확 살리고싶으신 분!!\n많이봐주세요~\n\n포스타입에서 고화질로 보실 수 있습니다^^\nhttps://t.co/IGGmRYn9fF\n\n재배포및 상업이용X\n\n#자료 #드로잉 https://t.co/JWO86LKCP1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			118
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "자료",
+					"indices": [
+						110,
+						113
+					]
+				},
+				{
+					"text": "드로잉",
+					"indices": [
+						114,
+						118
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IGGmRYn9fF",
+					"expanded_url": "http://posty.pe/12m8yk",
+					"display_url": "posty.pe/12m8yk",
+					"indices": [
+						73,
+						96
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1256190063383179300,
+					"id_str": "1256190063383179264",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/EW7iNQJUcAA5TP-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW7iNQJUcAA5TP-.jpg",
+					"url": "https://t.co/JWO86LKCP1",
+					"display_url": "pic.twitter.com/JWO86LKCP1",
+					"expanded_url": "https://twitter.com/Sartorial_E/status/1256190830043295745/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 506,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 892,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1523,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1256190063383179300,
+					"id_str": "1256190063383179264",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/EW7iNQJUcAA5TP-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW7iNQJUcAA5TP-.jpg",
+					"url": "https://t.co/JWO86LKCP1",
+					"display_url": "pic.twitter.com/JWO86LKCP1",
+					"expanded_url": "https://twitter.com/Sartorial_E/status/1256190830043295745/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 506,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 892,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1523,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1256189932600569900,
+					"id_str": "1256189932600569856",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/EW7iFo8UMAArr-y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW7iFo8UMAArr-y.jpg",
+					"url": "https://t.co/JWO86LKCP1",
+					"display_url": "pic.twitter.com/JWO86LKCP1",
+					"expanded_url": "https://twitter.com/Sartorial_E/status/1256190830043295745/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1448,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1256189932608950300,
+					"id_str": "1256189932608950280",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/EW7iFo-UEAgG20j.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW7iFo-UEAgG20j.jpg",
+					"url": "https://t.co/JWO86LKCP1",
+					"display_url": "pic.twitter.com/JWO86LKCP1",
+					"expanded_url": "https://twitter.com/Sartorial_E/status/1256190830043295745/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1448,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1256189932617359400,
+					"id_str": "1256189932617359361",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/EW7iFpAUYAEmRt7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW7iFpAUYAEmRt7.jpg",
+					"url": "https://t.co/JWO86LKCP1",
+					"display_url": "pic.twitter.com/JWO86LKCP1",
+					"expanded_url": "https://twitter.com/Sartorial_E/status/1256190830043295745/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1448,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1218158737409446000,
+			"id_str": "1218158737409445890",
+			"name": "24kt촉금(1920사이버망령)",
+			"screen_name": "Sartorial_E",
+			"location": "정장에대한집착은커져만갔다",
+			"description": "그림사담계/ 고전/1차                              \n질문함(아무거나질문ok) https://t.co/v9w1yFX3jv",
+			"url": "https://t.co/zIG8Fo4hl1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zIG8Fo4hl1",
+							"expanded_url": "https://maintain-sartorial-elegance.postype.com/",
+							"display_url": "…intain-sartorial-elegance.postype.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/v9w1yFX3jv",
+							"expanded_url": "https://peing.net/ko/sartorial_e",
+							"display_url": "peing.net/ko/sartorial_e",
+							"indices": [
+								57,
+								80
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3002,
+			"friends_count": 77,
+			"listed_count": 23,
+			"created_at": "Fri Jan 17 13:11:04 +0000 2020",
+			"favourites_count": 926,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5348,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1226166546931650560/0KCtpVHV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1226166546931650560/0KCtpVHV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1218158737409445890/1583080042",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2995,
+		"favorite_count": 3452,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "&lt;수트 드로잉 팁&gt;\n수트 그릴 때 핏 확 살리고싶으신 분!!\n많이봐주세요~\n\n포스타입에서 고화질로 보실 수 있습니다^^\nhttps://t.co/IGGmRYn9fF\n\n재배포및 상업이용X\n\n#자료 #드로잉 https://t.co/JWO86LKCP1"
+	}
+]
