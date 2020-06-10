4a1b757dcb2a934cf8e35859d7dacd2de74bbc23
ggongkui <32505785+ggongkui@users.noreply.github.com>
2020-06-10 09:15:31 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8768,
-	"created_at": "Mon Jun 01 00:15:30 +0000 2020",
+	"tweets": 8769,
+	"created_at": "Wed Jun 10 00:15:30 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_06.js
@@ -0,0 +1,164 @@
+Grailbird.data.tweets_2020_06 = 
+[
+	{
+		"created_at": "Tue Jun 09 12:06:52 +0000 2020",
+		"id": 1096006299735605200,
+		"id_str": "1270326493827100672",
+		"full_text": "국립현대미술관에서 정리한 한국 전통색상표라는데\n동양풍 캐디할때 이 표에서 색 뽑아쓰면 좋더라구요 딱 동양풍느낌나고\n저도 조선배경 캐디했던거 다 여기서 색뽑아서 배치했던 건데 좋아요 https://t.co/nTcbyzfdvQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1096006290789171200,
+					"id_str": "1096006290789171200",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/DzXL6iIVAAAR2kX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzXL6iIVAAAR2kX.jpg",
+					"url": "https://t.co/nTcbyzfdvQ",
+					"display_url": "pic.twitter.com/nTcbyzfdvQ",
+					"expanded_url": "https://twitter.com/yhr8746tmi2/status/1096006299735605248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 472,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 570,
+							"h": 821,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 570,
+							"h": 821,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1096006290789171200,
+					"id_str": "1096006290789171200",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/DzXL6iIVAAAR2kX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzXL6iIVAAAR2kX.jpg",
+					"url": "https://t.co/nTcbyzfdvQ",
+					"display_url": "pic.twitter.com/nTcbyzfdvQ",
+					"expanded_url": "https://twitter.com/yhr8746tmi2/status/1096006299735605248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 472,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 570,
+							"h": 821,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 570,
+							"h": 821,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 982646332744646700,
+			"id_str": "982646332744646656",
+			"name": "(🎊🎉클래스 오픈!!🎉🎊)김마웨 크루😷",
+			"screen_name": "kimmywaytmi2",
+			"location": "",
+			"description": "FUB FREE/Only OC/폰그림📱/Contact:DM or yhl8746@gmail.com/Do not use my creations without my permission.\n마웨님 혹은 크루님/트위터 확인 잘 못함/이별은 블언블~/Instagram : kimmyway1222",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4026,
+			"friends_count": 1825,
+			"listed_count": 10,
+			"created_at": "Sat Apr 07 15:48:37 +0000 2018",
+			"favourites_count": 4678,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23512,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1244998446869663745/yP-u2WaB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1244998446869663745/yP-u2WaB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/982646332744646656/1586882299",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4546,
+		"favorite_count": 3659,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "국립현대미술관에서 정리한 한국 전통색상표라는데\n동양풍 캐디할때 이 표에서 색 뽑아쓰면 좋더라구요 딱 동양풍느낌나고\n저도 조선배경 캐디했던거 다 여기서 색뽑아서 배치했던 건데 좋아요 https://t.co/nTcbyzfdvQ"
+	}
+]
