3c4a18c7e71bd5842788d3013c5412909fb50389
ggongkui <32505785+ggongkui@users.noreply.github.com>
2020-07-08 09:15:33 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8778,
-	"created_at": "Wed Jul 01 00:15:30 +0000 2020",
+	"tweets": 8779,
+	"created_at": "Wed Jul 08 00:15:31 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_07.js
@@ -0,0 +1,256 @@
+Grailbird.data.tweets_2020_07 = 
+[
+	{
+		"created_at": "Tue Jul 07 09:31:29 +0000 2020",
+		"id": 1191637921201152000,
+		"id_str": "1280434249305255937",
+		"full_text": "RT) 그동안 버클 그리는 거 은근 지겨우셨죠😆 벨트, 멜빵, 하네스 한번에 해결할 수 있는 버클 프리소스를 만들어 보았어요! 상업적 용도로도 사용OK입니다. 레이어 파츠별로 분리해놓았으니 아래 링크에서 받아주세요😘 https://t.co/pZGHwLdLcL https://t.co/sKhr6sky89",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			144
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/pZGHwLdLcL",
+					"expanded_url": "https://drive.google.com/open?id=1Ug1vtwkL5tKXuT1BWFsJLjRpv8w3OwBw",
+					"display_url": "drive.google.com/open?id=1Ug1vt…",
+					"indices": [
+						121,
+						144
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1191637910497349600,
+					"id_str": "1191637910497349632",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EImMXbQVUAAqoz7.png",
+					"media_url_https": "https://pbs.twimg.com/media/EImMXbQVUAAqoz7.png",
+					"url": "https://t.co/sKhr6sky89",
+					"display_url": "pic.twitter.com/sKhr6sky89",
+					"expanded_url": "https://twitter.com/NightWitch_0615/status/1191637921201152001/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 712,
+							"h": 712,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 712,
+							"h": 712,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1191637910497349600,
+					"id_str": "1191637910497349632",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EImMXbQVUAAqoz7.png",
+					"media_url_https": "https://pbs.twimg.com/media/EImMXbQVUAAqoz7.png",
+					"url": "https://t.co/sKhr6sky89",
+					"display_url": "pic.twitter.com/sKhr6sky89",
+					"expanded_url": "https://twitter.com/NightWitch_0615/status/1191637921201152001/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 712,
+							"h": 712,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 712,
+							"h": 712,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1191637910501511200,
+					"id_str": "1191637910501511168",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EImMXbRU0AAvawm.png",
+					"media_url_https": "https://pbs.twimg.com/media/EImMXbRU0AAvawm.png",
+					"url": "https://t.co/sKhr6sky89",
+					"display_url": "pic.twitter.com/sKhr6sky89",
+					"expanded_url": "https://twitter.com/NightWitch_0615/status/1191637921201152001/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 712,
+							"h": 712,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 712,
+							"h": 712,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1191637910497321000,
+					"id_str": "1191637910497320961",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EImMXbQU4AEXVaG.png",
+					"media_url_https": "https://pbs.twimg.com/media/EImMXbQU4AEXVaG.png",
+					"url": "https://t.co/sKhr6sky89",
+					"display_url": "pic.twitter.com/sKhr6sky89",
+					"expanded_url": "https://twitter.com/NightWitch_0615/status/1191637921201152001/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 712,
+							"h": 712,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 712,
+							"h": 712,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1177106296148856800,
+			"id_str": "1177106296148856832",
+			"name": "수강생 모집🔮밤마녀🧹",
+			"screen_name": "NightWitch_0615",
+			"location": "",
+			"description": "Illustrator\n수강상담 https://t.co/4qTgmxVVvZ\n❄️반동결❄️ (~7/6) 멘션 어려워요! 연락은 메일로\n💌 lolo_tn@naver.com",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/4qTgmxVVvZ",
+							"expanded_url": "https://fanding.kr/illust/nightwitch",
+							"display_url": "fanding.kr/illust/nightwi…",
+							"indices": [
+								17,
+								40
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 7999,
+			"friends_count": 465,
+			"listed_count": 28,
+			"created_at": "Thu Sep 26 06:23:14 +0000 2019",
+			"favourites_count": 10837,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7843,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1198610061917970433/frh3iqwS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1198610061917970433/frh3iqwS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1177106296148856832/1574605884",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7147,
+		"favorite_count": 6134,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "RT) 그동안 버클 그리는 거 은근 지겨우셨죠😆 벨트, 멜빵, 하네스 한번에 해결할 수 있는 버클 프리소스를 만들어 보았어요! 상업적 용도로도 사용OK입니다. 레이어 파츠별로 분리해놓았으니 아래 링크에서 받아주세요😘 https://t.co/pZGHwLdLcL https://t.co/sKhr6sky89"
+	}
+]
