d52093876b18c2293abb8a4cdad37355e2af6c0c
ggongkui <32505785+ggongkui@users.noreply.github.com>
2020-09-04 09:15:31 +0900

    Added 1 tweets

---
+++ data/js/tweets/2020_09.js
@@ -0,0 +1,194 @@
+Grailbird.data.tweets_2020_09 = 
+[
+	{
+		"created_at": "Thu Sep 03 23:03:22 +0000 2020",
+		"id": 1301527352057315300,
+		"id_str": "1301657065329311744",
+		"full_text": "가슴대란 https://t.co/14szXuu7Lx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			4
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1301526943804788700,
+					"id_str": "1301526943804788737",
+					"indices": [
+						5,
+						28
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1301526943804788737/pu/img/rtqKdremcgf4tE4B.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1301526943804788737/pu/img/rtqKdremcgf4tE4B.jpg",
+					"url": "https://t.co/14szXuu7Lx",
+					"display_url": "pic.twitter.com/14szXuu7Lx",
+					"expanded_url": "https://twitter.com/atarashiifut/status/1301527352057315328/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1301526943804788700,
+					"id_str": "1301526943804788737",
+					"indices": [
+						5,
+						28
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1301526943804788737/pu/img/rtqKdremcgf4tE4B.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1301526943804788737/pu/img/rtqKdremcgf4tE4B.jpg",
+					"url": "https://t.co/14szXuu7Lx",
+					"display_url": "pic.twitter.com/14szXuu7Lx",
+					"expanded_url": "https://twitter.com/atarashiifut/status/1301527352057315328/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 86167,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1301526943804788737/pu/vid/640x360/cFgcr_i9S0m8d5DR.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1301526943804788737/pu/vid/480x270/VokqUDVXfn2PDKoP.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1301526943804788737/pu/pl/jgONpKq_FrHMTZM5.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1301526943804788737/pu/vid/1280x720/uqjDlFAIxqbqFhwB.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1173855597453701000,
+			"id_str": "1173855597453701120",
+			"name": "렷카ગ브",
+			"screen_name": "atarashiifut",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 35864,
+			"friends_count": 0,
+			"listed_count": 52,
+			"created_at": "Tue Sep 17 07:06:04 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 43,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+			"profile_image_url_https": "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": true,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8449,
+		"favorite_count": 3083,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "가슴대란 https://t.co/14szXuu7Lx"
+	}
+]
