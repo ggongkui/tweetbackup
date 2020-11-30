8c8b1b8039ffb8a462db6885cfbdeb76895704f5
ggongkui <32505785+ggongkui@users.noreply.github.com>
2020-11-30 09:15:32 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8794,
-	"created_at": "Mon Oct 12 00:15:32 +0000 2020",
+	"tweets": 8795,
+	"created_at": "Mon Nov 30 00:15:30 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_11.js
@@ -0,0 +1,85 @@
+Grailbird.data.tweets_2020_11 = 
+[
+	{
+		"created_at": "Sun Nov 29 10:09:43 +0000 2020",
+		"id": 1332990111894573000,
+		"id_str": "1332990111894573057",
+		"full_text": "이게 혹시 마지막인가.\n그녀는 문득 의문했고, 살아오는 동안 두어 차례 같은 의문을 가졌던 순간들을 기억했다. 그때마다 짐작이 비껴가곤 했는데, 기어이 오늘인가.\n-한강 &lt;작별&gt;",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			105
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 815064414,
+			"id_str": "815064414",
+			"name": "?¿",
+			"screen_name": "PF_Si2",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 2,
+			"friends_count": 3,
+			"listed_count": 0,
+			"created_at": "Mon Sep 10 11:53:42 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1631,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1214959584642654208/2Ag5dpXE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1214959584642654208/2Ag5dpXE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/815064414/1578503912",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "이게 혹시 마지막인가.\n그녀는 문득 의문했고, 살아오는 동안 두어 차례 같은 의문을 가졌던 순간들을 기억했다. 그때마다 짐작이 비껴가곤 했는데, 기어이 오늘인가.\n-한강 &lt;작별&gt;"
+	}
+]
