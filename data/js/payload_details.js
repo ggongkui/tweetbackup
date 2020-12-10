950a20c0b71a48928d29109c3da8d03853b2db66
ggongkui <32505785+ggongkui@users.noreply.github.com>
2020-12-10 09:15:31 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8795,
-	"created_at": "Mon Nov 30 00:15:30 +0000 2020",
+	"tweets": 8796,
+	"created_at": "Thu Dec 10 00:15:30 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_12.js
@@ -0,0 +1,195 @@
+Grailbird.data.tweets_2020_12 = 
+[
+	{
+		"created_at": "Wed Dec 09 16:16:38 +0000 2020",
+		"id": 1114095063481503700,
+		"id_str": "1336706327876231169",
+		"full_text": "치카치카 연습 https://t.co/avVlTIDXjB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			7
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1114094996439699500,
+					"id_str": "1114094996439699456",
+					"indices": [
+						8,
+						31
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1114094996439699456/pu/img/xPUusEfdw9Obh-dZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1114094996439699456/pu/img/xPUusEfdw9Obh-dZ.jpg",
+					"url": "https://t.co/avVlTIDXjB",
+					"display_url": "pic.twitter.com/avVlTIDXjB",
+					"expanded_url": "https://twitter.com/orlorool/status/1114095063481503744/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1114094996439699500,
+					"id_str": "1114094996439699456",
+					"indices": [
+						8,
+						31
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1114094996439699456/pu/img/xPUusEfdw9Obh-dZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1114094996439699456/pu/img/xPUusEfdw9Obh-dZ.jpg",
+					"url": "https://t.co/avVlTIDXjB",
+					"display_url": "pic.twitter.com/avVlTIDXjB",
+					"expanded_url": "https://twitter.com/orlorool/status/1114095063481503744/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 20366,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1114094996439699456/pu/vid/360x640/j7vdpDc6LheJivPe.mp4?tag=8"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1114094996439699456/pu/pl/_6dzSP815Zg6HaY1.m3u8?tag=8"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1114094996439699456/pu/vid/720x1280/8DnNUrjF2xe6c662.mp4?tag=8"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1114094996439699456/pu/vid/320x568/crdnR2idUq6mSJaV.mp4?tag=8"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 890381937725984800,
+			"id_str": "890381937725984768",
+			"name": "리토🐈",
+			"screen_name": "orlorool",
+			"location": "",
+			"description": "애웅이를 도와주셨던 모든 분들께 항상 감사드려요. 동물계 위주로 맞팔합니다! 캔따개1, 2가 같이 굴리는 계정이에요👩‍💻👩‍🍳 #사지말고입양하세요",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1195,
+			"friends_count": 541,
+			"listed_count": 17,
+			"created_at": "Thu Jul 27 01:22:51 +0000 2017",
+			"favourites_count": 19464,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 17041,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1250358470307483649/FOLxSO2P_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1250358470307483649/FOLxSO2P_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/890381937725984768/1575039041",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 24396,
+		"favorite_count": 45556,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "치카치카 연습 https://t.co/avVlTIDXjB"
+	}
+]
