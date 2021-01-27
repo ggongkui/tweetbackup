155d217dad194158977e479e0ba40ac78001e4f6
ggongkui <32505785+ggongkui@users.noreply.github.com>
2021-01-27 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8799,
-	"created_at": "Thu Dec 17 00:15:29 +0000 2020",
+	"tweets": 8800,
+	"created_at": "Wed Jan 27 00:15:29 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_01.js
@@ -0,0 +1,272 @@
+Grailbird.data.tweets_2021_01 = 
+[
+	{
+		"created_at": "Tue Jan 26 14:12:10 +0000 2021",
+		"id": 1312075678926340000,
+		"id_str": "1354069622924840960",
+		"full_text": "상견니 웨이보 공계에서 공유했던 글.\n\n&lt;너무나 많은 죽음을 겪은 천윈루&gt; (오역, 의역 주의) https://t.co/34S1YZJlx4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			59
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1312075666712531000,
+					"id_str": "1312075666712530944",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/EjVt3gDVkAAg7gM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjVt3gDVkAAg7gM.jpg",
+					"url": "https://t.co/34S1YZJlx4",
+					"display_url": "pic.twitter.com/34S1YZJlx4",
+					"expanded_url": "https://twitter.com/tessa_gray123/status/1312075678926340096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1080,
+							"h": 1884,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 688,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 390,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1312075666712531000,
+					"id_str": "1312075666712530944",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/EjVt3gDVkAAg7gM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjVt3gDVkAAg7gM.jpg",
+					"url": "https://t.co/34S1YZJlx4",
+					"display_url": "pic.twitter.com/34S1YZJlx4",
+					"expanded_url": "https://twitter.com/tessa_gray123/status/1312075678926340096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1080,
+							"h": 1884,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 688,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 390,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1312075670168559600,
+					"id_str": "1312075670168559616",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/EjVt3s7UYAArHmJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjVt3s7UYAArHmJ.jpg",
+					"url": "https://t.co/34S1YZJlx4",
+					"display_url": "pic.twitter.com/34S1YZJlx4",
+					"expanded_url": "https://twitter.com/tessa_gray123/status/1312075678926340096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1752,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 740,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 419,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1312075673435992000,
+					"id_str": "1312075673435992065",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/EjVt35GVcAEGjCL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjVt35GVcAEGjCL.jpg",
+					"url": "https://t.co/34S1YZJlx4",
+					"display_url": "pic.twitter.com/34S1YZJlx4",
+					"expanded_url": "https://twitter.com/tessa_gray123/status/1312075678926340096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 450,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1632,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 794,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1312075676577484800,
+					"id_str": "1312075676577484801",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/EjVt4EzU0AEgjbC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjVt4EzU0AEgjbC.jpg",
+					"url": "https://t.co/34S1YZJlx4",
+					"display_url": "pic.twitter.com/34S1YZJlx4",
+					"expanded_url": "https://twitter.com/tessa_gray123/status/1312075678926340096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 700,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 397,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1852,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 902827959978868700,
+			"id_str": "902827959978868737",
+			"name": "아무말",
+			"screen_name": "tessa_gray123",
+			"location": "인생은 짧고 드라마는 많다",
+			"description": "여러 이야기들을 좋아함 / 스포 많음 / 여성 캐릭터 사랑함 / 다들 상견니 하세요",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 318,
+			"friends_count": 27,
+			"listed_count": 3,
+			"created_at": "Wed Aug 30 09:38:54 +0000 2017",
+			"favourites_count": 1089,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2985,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1348775148472553474/JmnNFi75_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1348775148472553474/JmnNFi75_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/902827959978868737/1609899389",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 347,
+		"favorite_count": 324,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "상견니 웨이보 공계에서 공유했던 글.\n\n&lt;너무나 많은 죽음을 겪은 천윈루&gt; (오역, 의역 주의) https://t.co/34S1YZJlx4"
+	}
+]
