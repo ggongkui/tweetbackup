97ced8f3bf1fb09d7f1d5c57cb3b3368c21eb22c
ggongkui <32505785+ggongkui@users.noreply.github.com>
2021-03-08 09:15:29 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8800,
-	"created_at": "Wed Jan 27 00:15:29 +0000 2021",
+	"tweets": 8801,
+	"created_at": "Mon Mar 08 00:15:29 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_03.js
@@ -0,0 +1,173 @@
+Grailbird.data.tweets_2021_03 = 
+[
+	{
+		"created_at": "Sun Mar 07 12:26:02 +0000 2021",
+		"id": 1359932132970307600,
+		"id_str": "1368538428330057733",
+		"full_text": "🔞꾸금 커미션 안내입니다.\n편하게 문의 주세요~! ^-^\nhttps://t.co/LVXWi3v548 https://t.co/V7VS0jmaiS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			55
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/LVXWi3v548",
+					"expanded_url": "https://posty.pe/juhpyx",
+					"display_url": "posty.pe/juhpyx",
+					"indices": [
+						32,
+						55
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1359931817021767700,
+					"id_str": "1359931817021767680",
+					"indices": [
+						56,
+						79
+					],
+					"media_url": "http://pbs.twimg.com/media/Et9yyEhVgAAo8lc.png",
+					"media_url_https": "https://pbs.twimg.com/media/Et9yyEhVgAAo8lc.png",
+					"url": "https://t.co/V7VS0jmaiS",
+					"display_url": "pic.twitter.com/V7VS0jmaiS",
+					"expanded_url": "https://twitter.com/Mv5Vs/status/1359932132970307584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1000,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 500,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1359931817021767700,
+					"id_str": "1359931817021767680",
+					"indices": [
+						56,
+						79
+					],
+					"media_url": "http://pbs.twimg.com/media/Et9yyEhVgAAo8lc.png",
+					"media_url_https": "https://pbs.twimg.com/media/Et9yyEhVgAAo8lc.png",
+					"url": "https://t.co/V7VS0jmaiS",
+					"display_url": "pic.twitter.com/V7VS0jmaiS",
+					"expanded_url": "https://twitter.com/Mv5Vs/status/1359932132970307584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1000,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 500,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1333783243540164600,
+			"id_str": "1333783243540164611",
+			"name": "완계",
+			"screen_name": "Mv5Vs",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 106,
+			"friends_count": 0,
+			"listed_count": 1,
+			"created_at": "Tue Dec 01 14:41:27 +0000 2020",
+			"favourites_count": 3,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 39,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1368512937128722435/_C2-QWGG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1368512937128722435/_C2-QWGG_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 171,
+		"favorite_count": 88,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "🔞꾸금 커미션 안내입니다.\n편하게 문의 주세요~! ^-^\nhttps://t.co/LVXWi3v548 https://t.co/V7VS0jmaiS"
+	}
+]
