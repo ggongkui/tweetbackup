c3fd25ec1a318b1c92de4d69db9bb74513ead9ce
ggongkui <32505785+ggongkui@users.noreply.github.com>
2021-03-16 09:15:30 +0900

    Added 1 tweets

--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,177 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Tue Mar 16 00:05:18 +0000 2021",
+		"id": 1358919391178494000,
+		"id_str": "1371613507855126529",
+		"full_text": "omfg have u heard of this site??? https://t.co/Z3JZV4FOfy it's not perfect but holy shit https://t.co/L5Vc9I20b2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Z3JZV4FOfy",
+					"expanded_url": "http://referenceangle.com/",
+					"display_url": "referenceangle.com",
+					"indices": [
+						34,
+						57
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1358919359473717200,
+					"id_str": "1358919359473717250",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/EtvZ9SPVEAIMUO-.png",
+					"media_url_https": "https://pbs.twimg.com/media/EtvZ9SPVEAIMUO-.png",
+					"url": "https://t.co/L5Vc9I20b2",
+					"display_url": "pic.twitter.com/L5Vc9I20b2",
+					"expanded_url": "https://twitter.com/geniusbeee/status/1358919391178493952/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 876,
+							"h": 655,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 876,
+							"h": 655,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 508,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1358919359473717200,
+					"id_str": "1358919359473717250",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/EtvZ9SPVEAIMUO-.png",
+					"media_url_https": "https://pbs.twimg.com/media/EtvZ9SPVEAIMUO-.png",
+					"url": "https://t.co/L5Vc9I20b2",
+					"display_url": "pic.twitter.com/L5Vc9I20b2",
+					"expanded_url": "https://twitter.com/geniusbeee/status/1358919391178493952/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 876,
+							"h": 655,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 876,
+							"h": 655,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 508,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 998806913998504000,
+			"id_str": "998806913998503936",
+			"name": "🤖✨a supervillain origin story✨🤖",
+			"screen_name": "geniusbeee",
+			"location": "",
+			"description": "@kikuhughes 's side account for pacific rim (2013) and batman villains (gay)\n29 // mixed yonsei // she/her",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2644,
+			"friends_count": 187,
+			"listed_count": 35,
+			"created_at": "Tue May 22 06:05:00 +0000 2018",
+			"favourites_count": 105617,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 45890,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1330642127877926912/RAIsQar5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1330642127877926912/RAIsQar5_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/998806913998503936/1606084781",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14998,
+		"favorite_count": 47990,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "omfg have u heard of this site??? https://t.co/Z3JZV4FOfy it's not perfect but holy shit https://t.co/L5Vc9I20b2"
+	},
+	{
 		"created_at": "Sun Mar 07 12:26:02 +0000 2021",
 		"id": 1359932132970307600,
 		"id_str": "1368538428330057733",
