78909523205161609960f847bd7ae9d955450cf9
ggongkui <32505785+ggongkui@users.noreply.github.com>
2021-04-07 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8806,
-	"created_at": "Fri Mar 26 00:15:34 +0000 2021",
+	"tweets": 8807,
+	"created_at": "Wed Apr 07 00:15:29 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_04.js
@@ -0,0 +1,213 @@
+Grailbird.data.tweets_2021_04 = 
+[
+	{
+		"created_at": "Tue Apr 06 13:11:24 +0000 2021",
+		"id": 1254706641833832400,
+		"id_str": "1379421482460176385",
+		"full_text": "지난 강좌에 이어서 이번에는 정장 악세사리에 대한 강좌입니다♡\n클래식한 정장 차림을 그리고 싶을 때 응용해보세요! https://t.co/B6kSWgPXbp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			63
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1254705773214527500,
+					"id_str": "1254705773214527490",
+					"indices": [
+						64,
+						87
+					],
+					"media_url": "http://pbs.twimg.com/media/EWmcQMYVcAIu3_5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EWmcQMYVcAIu3_5.jpg",
+					"url": "https://t.co/B6kSWgPXbp",
+					"display_url": "pic.twitter.com/B6kSWgPXbp",
+					"expanded_url": "https://twitter.com/spaniforce/status/1254706641833832448/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 681,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 482,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 681,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1254705773214527500,
+					"id_str": "1254705773214527490",
+					"indices": [
+						64,
+						87
+					],
+					"media_url": "http://pbs.twimg.com/media/EWmcQMYVcAIu3_5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EWmcQMYVcAIu3_5.jpg",
+					"url": "https://t.co/B6kSWgPXbp",
+					"display_url": "pic.twitter.com/B6kSWgPXbp",
+					"expanded_url": "https://twitter.com/spaniforce/status/1254706641833832448/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 681,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 482,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 681,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1254705890990538800,
+					"id_str": "1254705890990538753",
+					"indices": [
+						64,
+						87
+					],
+					"media_url": "http://pbs.twimg.com/media/EWmcXDIUwAE3OC9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EWmcXDIUwAE3OC9.jpg",
+					"url": "https://t.co/B6kSWgPXbp",
+					"display_url": "pic.twitter.com/B6kSWgPXbp",
+					"expanded_url": "https://twitter.com/spaniforce/status/1254706641833832448/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 482,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 681,
+							"h": 960,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 681,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1192001250956464000,
+			"id_str": "1192001250956464128",
+			"name": "🍄송파 애니포스 강좌봇🌱",
+			"screen_name": "spaniforce",
+			"location": "상담문의 ☎ 02) 416-6070",
+			"description": "송파 애니포스만화학원🌿 매주 다양한 자료와 강좌가 업로드 됩니다!💚",
+			"url": "https://t.co/pnFogELjK4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pnFogELjK4",
+							"expanded_url": "http://blog.naver.com/sp_aniforce",
+							"display_url": "blog.naver.com/sp_aniforce",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10677,
+			"friends_count": 9,
+			"listed_count": 1,
+			"created_at": "Wed Nov 06 08:50:29 +0000 2019",
+			"favourites_count": 15,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 119,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1211218183924805632/5Zi2Oz2D_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1211218183924805632/5Zi2Oz2D_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1192001250956464128/1577612234",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5224,
+		"favorite_count": 6038,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "지난 강좌에 이어서 이번에는 정장 악세사리에 대한 강좌입니다♡\n클래식한 정장 차림을 그리고 싶을 때 응용해보세요! https://t.co/B6kSWgPXbp"
+	}
+]
