2fa905c32899a00de468cbc3307702545e350e52
ggongkui <32505785+ggongkui@users.noreply.github.com>
2021-05-07 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8811,
-	"created_at": "Sun May 02 00:15:29 +0000 2021",
+	"tweets": 8812,
+	"created_at": "Fri May 07 00:15:29 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Thu May 06 02:23:28 +0000 2021",
+		"id": 1281423141768593400,
+		"id_str": "1390130061181964294",
+		"full_text": "작문법을 공부하고 싶다면 이 분이 쓰신 걸 보는 게 최고일듯. 무협지 표현을 빌리자면 무림에서 으뜸으로 꼽는 문파의 맹주가 손수 비급서 들고 와서 가르쳐주는 정도.\nhttps://t.co/J7HOUDSXNf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			115
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/J7HOUDSXNf",
+					"expanded_url": "http://posty.pe/bembwp",
+					"display_url": "posty.pe/bembwp",
+					"indices": [
+						92,
+						115
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1159950894730866700,
+			"id_str": "1159950894730866689",
+			"name": "에클레어",
+			"screen_name": "cp1348",
+			"location": "",
+			"description": "자기소개를 써봅시다:3c",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 94,
+			"friends_count": 103,
+			"listed_count": 0,
+			"created_at": "Fri Aug 09 22:13:38 +0000 2019",
+			"favourites_count": 20687,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11626,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1387688123920879619/1JNhfCDn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1387688123920879619/1JNhfCDn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1159950894730866689/1619096653",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7746,
+		"favorite_count": 6715,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "작문법을 공부하고 싶다면 이 분이 쓰신 걸 보는 게 최고일듯. 무협지 표현을 빌리자면 무림에서 으뜸으로 꼽는 문파의 맹주가 손수 비급서 들고 와서 가르쳐주는 정도.\nhttps://t.co/J7HOUDSXNf"
+	},
+	{
 		"created_at": "Sat May 01 09:10:14 +0000 2021",
 		"id": 1388343929305587700,
 		"id_str": "1388420488544669697",
