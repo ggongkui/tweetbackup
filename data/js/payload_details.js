97af35bfcb706e7d90239faba28207527105b032
ggongkui <32505785+ggongkui@users.noreply.github.com>
2021-05-13 09:15:30 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8812,
-	"created_at": "Fri May 07 00:15:29 +0000 2021",
+	"tweets": 8815,
+	"created_at": "Thu May 13 00:15:29 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,568 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Wed May 12 18:02:29 +0000 2021",
+		"id": 1388852252379598800,
+		"id_str": "1392540700411973635",
+		"full_text": "혹시 운동 처음이시거나 초보분들 홈트 하고 싶은데 어떤 루틴으로 해야 하는지 모르겠으면 저랑 같이 이거 해요! 이분 블로그 가면 1달 치 루틴이 나와있고 링크 누르면 영상으로 이동해요 매일 30분 내외로 구성되어 있어요 짧고 효과 좋은 게 특징이에요!\nhttps://t.co/vm3mXgsbGF https://t.co/AbMKKfXKKA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			164
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vm3mXgsbGF",
+					"expanded_url": "https://m.blog.naver.com/PostView.nhn?blogId=dllwllsll&logNo=222330098320&proxyReferer=https:%2F%2Fblog.naver.com%2Fdllwllsll%2F222330098320",
+					"display_url": "m.blog.naver.com/PostView.nhn?b…",
+					"indices": [
+						141,
+						164
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1388852246192877600,
+					"id_str": "1388852246192877573",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/media/E0YxwmvUcAU0UN5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E0YxwmvUcAU0UN5.jpg",
+					"url": "https://t.co/AbMKKfXKKA",
+					"display_url": "pic.twitter.com/AbMKKfXKKA",
+					"expanded_url": "https://twitter.com/Gaenova/status/1388852252379598852/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 515,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 866,
+							"h": 1144,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 866,
+							"h": 1144,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1388852246192877600,
+					"id_str": "1388852246192877573",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/media/E0YxwmvUcAU0UN5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E0YxwmvUcAU0UN5.jpg",
+					"url": "https://t.co/AbMKKfXKKA",
+					"display_url": "pic.twitter.com/AbMKKfXKKA",
+					"expanded_url": "https://twitter.com/Gaenova/status/1388852252379598852/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 515,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 866,
+							"h": 1144,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 866,
+							"h": 1144,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1264425413758972000,
+			"id_str": "1264425413758971904",
+			"name": "개노바",
+			"screen_name": "Gaenova",
+			"location": "",
+			"description": "만년필, 잉크, 필사 좋아하는 계정이지만 만년필 사진이 9할 얼레벌레 시필 헤더는 델핀님💓",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 508,
+			"friends_count": 251,
+			"listed_count": 5,
+			"created_at": "Sun May 24 05:18:10 +0000 2020",
+			"favourites_count": 41667,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6855,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1364418473472843780/0YN8NDtT_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1364418473472843780/0YN8NDtT_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1264425413758971904/1617017031",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11608,
+		"favorite_count": 9963,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "혹시 운동 처음이시거나 초보분들 홈트 하고 싶은데 어떤 루틴으로 해야 하는지 모르겠으면 저랑 같이 이거 해요! 이분 블로그 가면 1달 치 루틴이 나와있고 링크 누르면 영상으로 이동해요 매일 30분 내외로 구성되어 있어요 짧고 효과 좋은 게 특징이에요!\nhttps://t.co/vm3mXgsbGF https://t.co/AbMKKfXKKA"
+	},
+	{
+		"created_at": "Wed May 12 17:28:51 +0000 2021",
+		"id": 1361176823611056000,
+		"id_str": "1392532234805022720",
+		"full_text": "https://t.co/DHgL7FgfIE\n만화는 재능이고 이론으로 설명할 수 없다고 생각했는데 그걸 해낸 만화.\n보고 있으면 역시 천재란 생각밖에 안드는...\n만화콘티 작법서를 찾고 계신다면 이것만 보셔도 됩니다.\n쌩초보보다 만화를 이미 그리고 있는데,(프로든 아마추어든) 더 잘 그리고 싶은 분들에게 추천.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			173
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/DHgL7FgfIE",
+					"expanded_url": "https://posty.pe/s3plyj7",
+					"display_url": "posty.pe/s3plyj7",
+					"indices": [
+						0,
+						23
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 714370951098466300,
+			"id_str": "714370951098466306",
+			"name": "튼튼",
+			"screen_name": "illyslilly",
+			"location": "",
+			"description": "리디북스《짝사랑의 스킬》@carboNo_non 코미코 《위장여친》 봐주세요♡ / 백합, GL / 불펌, 상업적 이용 금지 / ⓒillyslilly All Rights Reserved",
+			"url": "https://t.co/UhLKykNYJz",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UhLKykNYJz",
+							"expanded_url": "http://lillyslilly.postype.com",
+							"display_url": "lillyslilly.postype.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17028,
+			"friends_count": 35,
+			"listed_count": 44,
+			"created_at": "Mon Mar 28 08:38:01 +0000 2016",
+			"favourites_count": 1597,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3037,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1366782831926472706/RlVE6tYm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1366782831926472706/RlVE6tYm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/714370951098466306/1594434458",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6643,
+		"favorite_count": 5550,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "https://t.co/DHgL7FgfIE\n만화는 재능이고 이론으로 설명할 수 없다고 생각했는데 그걸 해낸 만화.\n보고 있으면 역시 천재란 생각밖에 안드는...\n만화콘티 작법서를 찾고 계신다면 이것만 보셔도 됩니다.\n쌩초보보다 만화를 이미 그리고 있는데,(프로든 아마추어든) 더 잘 그리고 싶은 분들에게 추천."
+	},
+	{
+		"created_at": "Wed May 12 17:28:30 +0000 2021",
+		"id": 1313481757610606600,
+		"id_str": "1392532148045770754",
+		"full_text": "연출 어떻게 하냐는 질문을 받을 때마다 어디서부터 대답해야할지 몰라 생략했는데.. 가장 기본적으로 지키는 요소만 정리해봤습니다.. 페이지 만화 기준입니다. 개인적인 견해가 첨부되어 있습니다. https://t.co/aZHn6bQvur",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			106
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1313480712671068200,
+					"id_str": "1313480712671068160",
+					"indices": [
+						107,
+						130
+					],
+					"media_url": "http://pbs.twimg.com/media/Ejprv8IWAAAG13a.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ejprv8IWAAAG13a.jpg",
+					"url": "https://t.co/aZHn6bQvur",
+					"display_url": "pic.twitter.com/aZHn6bQvur",
+					"expanded_url": "https://twitter.com/MABELMINE_/status/1313481757610606593/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 434,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1226,
+							"h": 1921,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 766,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1313480712671068200,
+					"id_str": "1313480712671068160",
+					"indices": [
+						107,
+						130
+					],
+					"media_url": "http://pbs.twimg.com/media/Ejprv8IWAAAG13a.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ejprv8IWAAAG13a.jpg",
+					"url": "https://t.co/aZHn6bQvur",
+					"display_url": "pic.twitter.com/aZHn6bQvur",
+					"expanded_url": "https://twitter.com/MABELMINE_/status/1313481757610606593/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 434,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1226,
+							"h": 1921,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 766,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1313480771735347200,
+					"id_str": "1313480771735347201",
+					"indices": [
+						107,
+						130
+					],
+					"media_url": "http://pbs.twimg.com/media/EjprzYKXYAEgKZ8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjprzYKXYAEgKZ8.jpg",
+					"url": "https://t.co/aZHn6bQvur",
+					"display_url": "pic.twitter.com/aZHn6bQvur",
+					"expanded_url": "https://twitter.com/MABELMINE_/status/1313481757610606593/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1622,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 538,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 950,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1313480787178795000,
+					"id_str": "1313480787178795008",
+					"indices": [
+						107,
+						130
+					],
+					"media_url": "http://pbs.twimg.com/media/Ejpr0RsXsAAYwth.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ejpr0RsXsAAYwth.jpg",
+					"url": "https://t.co/aZHn6bQvur",
+					"display_url": "pic.twitter.com/aZHn6bQvur",
+					"expanded_url": "https://twitter.com/MABELMINE_/status/1313481757610606593/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 648,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 367,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1106,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1313480799732281300,
+					"id_str": "1313480799732281351",
+					"indices": [
+						107,
+						130
+					],
+					"media_url": "http://pbs.twimg.com/media/Ejpr1AdWsAcu4yE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ejpr1AdWsAcu4yE.jpg",
+					"url": "https://t.co/aZHn6bQvur",
+					"display_url": "pic.twitter.com/aZHn6bQvur",
+					"expanded_url": "https://twitter.com/MABELMINE_/status/1313481757610606593/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1422,
+							"h": 2000,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 853,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 483,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3065873773,
+			"id_str": "3065873773",
+			"name": "연온",
+			"screen_name": "MABELMINE_",
+			"location": "",
+			"description": "🔕 • silirit@naver.com",
+			"url": "https://t.co/22oQTdCQqb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/22oQTdCQqb",
+							"expanded_url": "https://antarcticbase.postype.com/",
+							"display_url": "antarcticbase.postype.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 46191,
+			"friends_count": 87,
+			"listed_count": 292,
+			"created_at": "Sat Mar 07 04:16:28 +0000 2015",
+			"favourites_count": 658,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1016,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1369065663931641864/ogpo5fUV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1369065663931641864/ogpo5fUV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3065873773/1615245504",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9096,
+		"favorite_count": 10880,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "연출 어떻게 하냐는 질문을 받을 때마다 어디서부터 대답해야할지 몰라 생략했는데.. 가장 기본적으로 지키는 요소만 정리해봤습니다.. 페이지 만화 기준입니다. 개인적인 견해가 첨부되어 있습니다. https://t.co/aZHn6bQvur"
+	},
+	{
 		"created_at": "Thu May 06 02:23:28 +0000 2021",
 		"id": 1281423141768593400,
 		"id_str": "1390130061181964294",
