5eb77c0e5ca01275f708ac6ece2b04f91f316396
ggongkui <32505785+ggongkui@users.noreply.github.com>
2021-06-27 09:15:29 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8818,
-	"created_at": "Mon Jun 21 00:15:29 +0000 2021",
+	"tweets": 8819,
+	"created_at": "Sun Jun 27 00:15:29 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,225 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Sat Jun 26 16:25:19 +0000 2021",
+		"id": 1406618814465278000,
+		"id_str": "1408823700325953538",
+		"full_text": "#전공을_살려_유익한_말을_해보자\n\n이 사진을                            요런 색감이 되려면\n포토샵에서 camera raw 필터라는 친구가 있는데 그 친구로 색감 조절하면서 명도 조절을 할 수 있습니다!! 그리고 명료도나 채도, 활기 조절도 다 돼서 저는 많이 써요 &gt;____&lt; 저 디자인 작품에서는 https://t.co/qNCXx5jG9L",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			185
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "전공을_살려_유익한_말을_해보자",
+					"indices": [
+						0,
+						18
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1406617433637486600,
+					"id_str": "1406617433637486594",
+					"indices": [
+						186,
+						209
+					],
+					"media_url": "http://pbs.twimg.com/media/E4VPGnLVgAIu1BN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4VPGnLVgAIu1BN.jpg",
+					"url": "https://t.co/qNCXx5jG9L",
+					"display_url": "pic.twitter.com/qNCXx5jG9L",
+					"expanded_url": "https://twitter.com/__co_ring/status/1406618814465277961/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1537,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1406617433637486600,
+					"id_str": "1406617433637486594",
+					"indices": [
+						186,
+						209
+					],
+					"media_url": "http://pbs.twimg.com/media/E4VPGnLVgAIu1BN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4VPGnLVgAIu1BN.jpg",
+					"url": "https://t.co/qNCXx5jG9L",
+					"display_url": "pic.twitter.com/qNCXx5jG9L",
+					"expanded_url": "https://twitter.com/__co_ring/status/1406618814465277961/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1537,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1406617450397855700,
+					"id_str": "1406617450397855756",
+					"indices": [
+						186,
+						209
+					],
+					"media_url": "http://pbs.twimg.com/media/E4VPHlnUcAw3AX3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4VPHlnUcAw3AX3.jpg",
+					"url": "https://t.co/qNCXx5jG9L",
+					"display_url": "pic.twitter.com/qNCXx5jG9L",
+					"expanded_url": "https://twitter.com/__co_ring/status/1406618814465277961/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1097,
+							"h": 1509,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 872,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 494,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1361331374817349600,
+			"id_str": "1361331374817349635",
+			"name": "(타투스티커 2차 판매) 코링",
+			"screen_name": "__co_ring",
+			"location": "鬼殺隊 | 20⬆️",
+			"description": "❤時透 無一郎❤   |   #코링_디자인",
+			"url": "https://t.co/GZpgQiBa77",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GZpgQiBa77",
+							"expanded_url": "http://asked.kr/co_ring",
+							"display_url": "asked.kr/co_ring",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 396,
+			"friends_count": 383,
+			"listed_count": 12,
+			"created_at": "Mon Feb 15 15:08:00 +0000 2021",
+			"favourites_count": 10712,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4429,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1408438913966825480/6eb0t1kg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1408438913966825480/6eb0t1kg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1361331374817349635/1615122761",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6382,
+		"favorite_count": 6848,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#전공을_살려_유익한_말을_해보자\n\n이 사진을                            요런 색감이 되려면\n포토샵에서 camera raw 필터라는 친구가 있는데 그 친구로 색감 조절하면서 명도 조절을 할 수 있습니다!! 그리고 명료도나 채도, 활기 조절도 다 돼서 저는 많이 써요 &gt;____&lt; 저 디자인 작품에서는 https://t.co/qNCXx5jG9L"
+	},
+	{
 		"created_at": "Sun Jun 20 14:38:21 +0000 2021",
 		"id": 1406525633354555400,
 		"id_str": "1406622455112757257",
