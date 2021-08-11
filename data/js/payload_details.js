d8fad631ce7cf706031c12f2a9957238826020ca
ggongkui <32505785+ggongkui@users.noreply.github.com>
2021-08-11 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8820,
-	"created_at": "Mon Jul 12 00:15:29 +0000 2021",
+	"tweets": 8821,
+	"created_at": "Wed Aug 11 00:15:29 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_08.js
@@ -0,0 +1,250 @@
+Grailbird.data.tweets_2021_08 = 
+[
+	{
+		"created_at": "Tue Aug 10 23:58:09 +0000 2021",
+		"id": 1425115333719838700,
+		"id_str": "1425245115417653249",
+		"full_text": "広がった袖を描くのに毎回苦労していて、良い資料になりそうな服で色々写真撮ったのでおすそ分け。トレースとかに使ってもらってOKです。（縫製によって出るシワも違うだろうけど…） https://t.co/Kf21eX8pvM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			86
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1425113534468223000,
+					"id_str": "1425113534468222982",
+					"indices": [
+						87,
+						110
+					],
+					"media_url": "http://pbs.twimg.com/media/E8cFNYKUcAYXBD_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8cFNYKUcAYXBD_.jpg",
+					"url": "https://t.co/Kf21eX8pvM",
+					"display_url": "pic.twitter.com/Kf21eX8pvM",
+					"expanded_url": "https://twitter.com/HiakiWoo/status/1425115333719838720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 609,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 896,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 896,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1425113534468223000,
+					"id_str": "1425113534468222982",
+					"indices": [
+						87,
+						110
+					],
+					"media_url": "http://pbs.twimg.com/media/E8cFNYKUcAYXBD_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8cFNYKUcAYXBD_.jpg",
+					"url": "https://t.co/Kf21eX8pvM",
+					"display_url": "pic.twitter.com/Kf21eX8pvM",
+					"expanded_url": "https://twitter.com/HiakiWoo/status/1425115333719838720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 609,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 896,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 896,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1425113600251678700,
+					"id_str": "1425113600251678733",
+					"indices": [
+						87,
+						110
+					],
+					"media_url": "http://pbs.twimg.com/media/E8cFRNOUUA0t9j5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8cFRNOUUA0t9j5.jpg",
+					"url": "https://t.co/Kf21eX8pvM",
+					"display_url": "pic.twitter.com/Kf21eX8pvM",
+					"expanded_url": "https://twitter.com/HiakiWoo/status/1425115333719838720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1425113630962421800,
+					"id_str": "1425113630962421760",
+					"indices": [
+						87,
+						110
+					],
+					"media_url": "http://pbs.twimg.com/media/E8cFS_oVEAADald.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8cFS_oVEAADald.jpg",
+					"url": "https://t.co/Kf21eX8pvM",
+					"display_url": "pic.twitter.com/Kf21eX8pvM",
+					"expanded_url": "https://twitter.com/HiakiWoo/status/1425115333719838720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 756,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 514,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 756,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 53585499,
+			"id_str": "53585499",
+			"name": "うー",
+			"screen_name": "HiakiWoo",
+			"location": "奈良県",
+			"description": "しがない会社員。好物は豆腐/ﾎﾟﾀｰｼﾞｭｽｰﾌﾟ/鍋/ﾁｯﾌﾟｽﾀｰ（ｺﾝｿﾒ）/焼肉さん太郎/ｸｯﾋﾟｰﾗﾑﾈ/天津飯/麻婆豆腐/ﾊｯﾋﾟｰｺｰﾗ/ﾌﾞﾗｯｸｻﾝﾀﾞｰ/ﾛｰﾙｷｬﾍﾞﾂ/わたがし",
+			"url": "http://t.co/6QGPs1qRqx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/6QGPs1qRqx",
+							"expanded_url": "http://hiakiwoo.fc2web.com/",
+							"display_url": "hiakiwoo.fc2web.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4262,
+			"friends_count": 27,
+			"listed_count": 79,
+			"created_at": "Sat Jul 04 03:45:37 +0000 2009",
+			"favourites_count": 106,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6888,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/602634510/twitterProfilePhoto_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/602634510/twitterProfilePhoto_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/53585499/1411891661",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7542,
+		"favorite_count": 30607,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "広がった袖を描くのに毎回苦労していて、良い資料になりそうな服で色々写真撮ったのでおすそ分け。トレースとかに使ってもらってOKです。（縫製によって出るシワも違うだろうけど…） https://t.co/Kf21eX8pvM"
+	}
+]
