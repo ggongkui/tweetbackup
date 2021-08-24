0914cdf2a335610fab4acba3e3ef7b9b7a557831
ggongkui <32505785+ggongkui@users.noreply.github.com>
2021-08-24 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8821,
-	"created_at": "Wed Aug 11 00:15:29 +0000 2021",
+	"tweets": 8822,
+	"created_at": "Tue Aug 24 00:15:29 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Mon Aug 23 11:18:07 +0000 2021",
+		"id": 1429764888230994000,
+		"id_str": "1429764888230993920",
+		"full_text": "https://t.co/OG78uFQmeQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1429764847873400800,
+					"id_str": "1429764847873400832",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/E9eLjc3VoAArVg-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9eLjc3VoAArVg-.jpg",
+					"url": "https://t.co/OG78uFQmeQ",
+					"display_url": "pic.twitter.com/OG78uFQmeQ",
+					"expanded_url": "https://twitter.com/PF_Si2/status/1429764888230993920/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 568,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 322,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 970,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1429764847873400800,
+					"id_str": "1429764847873400832",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/E9eLjc3VoAArVg-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9eLjc3VoAArVg-.jpg",
+					"url": "https://t.co/OG78uFQmeQ",
+					"display_url": "pic.twitter.com/OG78uFQmeQ",
+					"expanded_url": "https://twitter.com/PF_Si2/status/1429764888230993920/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 568,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 322,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 970,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1429764860829573000,
+					"id_str": "1429764860829573128",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/E9eLkNIVIAgdng0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9eLkNIVIAgdng0.jpg",
+					"url": "https://t.co/OG78uFQmeQ",
+					"display_url": "pic.twitter.com/OG78uFQmeQ",
+					"expanded_url": "https://twitter.com/PF_Si2/status/1429764888230993920/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 492,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 839,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 279,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1429764872200360000,
+					"id_str": "1429764872200359942",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/E9eLk3fVkAYMS3d.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9eLk3fVkAYMS3d.jpg",
+					"url": "https://t.co/OG78uFQmeQ",
+					"display_url": "pic.twitter.com/OG78uFQmeQ",
+					"expanded_url": "https://twitter.com/PF_Si2/status/1429764888230993920/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 430,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 759,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1296,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 815064414,
+			"id_str": "815064414",
+			"name": "?¿",
+			"screen_name": "PF_Si2",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 2,
+			"friends_count": 4,
+			"listed_count": 0,
+			"created_at": "Mon Sep 10 11:53:42 +0000 2012",
+			"favourites_count": 119,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1589,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1214959584642654208/2Ag5dpXE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1214959584642654208/2Ag5dpXE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/815064414/1578503912",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/OG78uFQmeQ"
+	},
+	{
 		"created_at": "Tue Aug 10 23:58:09 +0000 2021",
 		"id": 1425115333719838700,
 		"id_str": "1425245115417653249",
