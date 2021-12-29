945b15226941dffdd05bcae202deaf13d7c99055
ggongkui <32505785+ggongkui@users.noreply.github.com>
2021-12-29 09:15:30 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8849,
-	"created_at": "Wed Nov 10 00:15:30 +0000 2021",
+	"tweets": 8851,
+	"created_at": "Wed Dec 29 00:15:29 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_12.js
@@ -0,0 +1,483 @@
+Grailbird.data.tweets_2021_12 = 
+[
+	{
+		"created_at": "Tue Dec 28 23:22:04 +0000 2021",
+		"id": 1186498244202774500,
+		"id_str": "1475970332964323328",
+		"full_text": "금속 문양 그리기 튜토리얼입니다.\n재질 표현을 어려워하시는 분들을 위해 이번에도 채색 위주로 내용을 구성했습니다. \nHow to Draw Antique pattern👑 https://t.co/sbOWkmwt3k",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1186498229099065300,
+					"id_str": "1186498229099065344",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EHdJ2mAUcAAGGbY.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EHdJ2mAUcAAGGbY.jpg",
+					"url": "https://t.co/sbOWkmwt3k",
+					"display_url": "pic.twitter.com/sbOWkmwt3k",
+					"expanded_url": "https://twitter.com/p2nki/status/1186498244202774528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1186498229099065300,
+					"id_str": "1186498229099065344",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EHdJ2mAUcAAGGbY.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EHdJ2mAUcAAGGbY.jpg",
+					"url": "https://t.co/sbOWkmwt3k",
+					"display_url": "pic.twitter.com/sbOWkmwt3k",
+					"expanded_url": "https://twitter.com/p2nki/status/1186498244202774528/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EHdJ2mAUcAAGGbY.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 839142692785369100,
+			"id_str": "839142692785369089",
+			"name": "SOLRAKA💙솔라카 / 콜로소 강의 오픈",
+			"screen_name": "p2nki",
+			"location": "etnoc@naver.com",
+			"description": "illustratorㅣ메가스터디 일러스트 강사ㅣ레이어 효과로 완성하는 캐릭터 컬러링 출간ㅣ콜로소 온라인 강의 https://t.co/gtB7vReqdF",
+			"url": "https://t.co/Ja3W9MvGTE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ja3W9MvGTE",
+							"expanded_url": "https://cafe.naver.com/megaacademygame/162",
+							"display_url": "cafe.naver.com/megaacademygam…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/gtB7vReqdF",
+							"expanded_url": "http://bit.ly/3iRh9Lw",
+							"display_url": "bit.ly/3iRh9Lw",
+							"indices": [
+								61,
+								84
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 77194,
+			"friends_count": 197,
+			"listed_count": 0,
+			"created_at": "Tue Mar 07 15:56:23 +0000 2017",
+			"favourites_count": 1544,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1196,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1315721161318100993/GdQ2Q6Jg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1315721161318100993/GdQ2Q6Jg_normal.jpg",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5775,
+		"favorite_count": 8729,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "금속 문양 그리기 튜토리얼입니다.\n재질 표현을 어려워하시는 분들을 위해 이번에도 채색 위주로 내용을 구성했습니다. \nHow to Draw Antique pattern👑 https://t.co/sbOWkmwt3k"
+	},
+	{
+		"created_at": "Tue Dec 28 23:21:56 +0000 2021",
+		"id": 1186545337667686400,
+		"id_str": "1475970301901291521",
+		"full_text": "자주 사용하지만 어려운 손동작들의 예시자료입니다! 🙌 https://t.co/2nUc7FmJNU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			29
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1186544124792459300,
+					"id_str": "1186544124792459264",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/EHdzmEyU0AA37l4.png",
+					"media_url_https": "https://pbs.twimg.com/media/EHdzmEyU0AA37l4.png",
+					"url": "https://t.co/2nUc7FmJNU",
+					"display_url": "pic.twitter.com/2nUc7FmJNU",
+					"expanded_url": "https://twitter.com/ANIFORCE_PUSAN/status/1186545337667686400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 661,
+							"h": 869,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 517,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 661,
+							"h": 869,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1186544124792459300,
+					"id_str": "1186544124792459264",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/EHdzmEyU0AA37l4.png",
+					"media_url_https": "https://pbs.twimg.com/media/EHdzmEyU0AA37l4.png",
+					"url": "https://t.co/2nUc7FmJNU",
+					"display_url": "pic.twitter.com/2nUc7FmJNU",
+					"expanded_url": "https://twitter.com/ANIFORCE_PUSAN/status/1186545337667686400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 661,
+							"h": 869,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 517,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 661,
+							"h": 869,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1186544251594625000,
+					"id_str": "1186544251594625024",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/EHdztdKUUAAe99l.png",
+					"media_url_https": "https://pbs.twimg.com/media/EHdztdKUUAAe99l.png",
+					"url": "https://t.co/2nUc7FmJNU",
+					"display_url": "pic.twitter.com/2nUc7FmJNU",
+					"expanded_url": "https://twitter.com/ANIFORCE_PUSAN/status/1186545337667686400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 629,
+							"h": 899,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 476,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 629,
+							"h": 899,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1186544434017497000,
+					"id_str": "1186544434017497090",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/EHdz4EvUcAI-R7m.png",
+					"media_url_https": "https://pbs.twimg.com/media/EHdz4EvUcAI-R7m.png",
+					"url": "https://t.co/2nUc7FmJNU",
+					"display_url": "pic.twitter.com/2nUc7FmJNU",
+					"expanded_url": "https://twitter.com/ANIFORCE_PUSAN/status/1186545337667686400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 661,
+							"h": 899,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 661,
+							"h": 899,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1186544705804230700,
+					"id_str": "1186544705804230656",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/EHd0H5OU4AAM5pb.png",
+					"media_url_https": "https://pbs.twimg.com/media/EHd0H5OU4AAM5pb.png",
+					"url": "https://t.co/2nUc7FmJNU",
+					"display_url": "pic.twitter.com/2nUc7FmJNU",
+					"expanded_url": "https://twitter.com/ANIFORCE_PUSAN/status/1186545337667686400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 661,
+							"h": 899,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 661,
+							"h": 899,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1099957041911582700,
+			"id_str": "1099957041911582720",
+			"name": "🐈부산 애니포스 강좌봇🐇",
+			"screen_name": "ANIFORCE_PUSAN",
+			"location": "대한민국 부산",
+			"description": "부산 동래 애니포스 / 해운대 애니포스의 공동 그림 강좌 계정입니다. \n인체해부학,클립스튜디오, 캐릭터,이메레스 등 다양한 강좌들을 선보입니다.",
+			"url": "https://t.co/deqsksa0rQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/deqsksa0rQ",
+							"expanded_url": "http://www.art-prism.com",
+							"display_url": "art-prism.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14324,
+			"friends_count": 9,
+			"listed_count": 71,
+			"created_at": "Mon Feb 25 08:59:29 +0000 2019",
+			"favourites_count": 85,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 64,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1143755488422576130/is2XHmZn_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1143755488422576130/is2XHmZn_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1099957041911582720/1580394776",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10229,
+		"favorite_count": 14851,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "자주 사용하지만 어려운 손동작들의 예시자료입니다! 🙌 https://t.co/2nUc7FmJNU"
+	}
+]
