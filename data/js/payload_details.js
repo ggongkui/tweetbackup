93f98353021afadaa116acb1ea8ffa3f6e6bda85
ggongkui <32505785+ggongkui@users.noreply.github.com>
2022-01-18 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8853,
-	"created_at": "Fri Jan 07 00:15:29 +0000 2022",
+	"tweets": 8854,
+	"created_at": "Tue Jan 18 00:15:29 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Mon Jan 17 23:05:24 +0000 2022",
+		"id": 1284932029453176800,
+		"id_str": "1483213898853793792",
+		"full_text": "새벽에 몰래웃다가 침흘릴뻔햇내 왇떠 빸!!!!!!!!!! https://t.co/bIKJr1b5az",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			31
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1284931941339291600,
+					"id_str": "1284931941339291648",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1284931941339291648/pu/img/IDsQQw6VbDGPqJgm.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1284931941339291648/pu/img/IDsQQw6VbDGPqJgm.jpg",
+					"url": "https://t.co/bIKJr1b5az",
+					"display_url": "pic.twitter.com/bIKJr1b5az",
+					"expanded_url": "https://twitter.com/TARU__9/status/1284932029453176833/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 288,
+							"h": 360,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 288,
+							"h": 360,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 288,
+							"h": 360,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1284931941339291600,
+					"id_str": "1284931941339291648",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1284931941339291648/pu/img/IDsQQw6VbDGPqJgm.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1284931941339291648/pu/img/IDsQQw6VbDGPqJgm.jpg",
+					"url": "https://t.co/bIKJr1b5az",
+					"display_url": "pic.twitter.com/bIKJr1b5az",
+					"expanded_url": "https://twitter.com/TARU__9/status/1284932029453176833/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 288,
+							"h": 360,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 288,
+							"h": 360,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 288,
+							"h": 360,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							5
+						],
+						"duration_millis": 18640,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1284931941339291648/pu/pl/ianFLFudrE6daIdt.m3u8?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1284931941339291648/pu/vid/288x360/V13P2todzolWHF92.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 766627363870257200,
+			"id_str": "766627363870257152",
+			"name": "참아 이쁜아",
+			"screen_name": "TARU__9",
+			"location": "",
+			"description": "LOST ARK ｜ 체블, 미블 ｜ 💗트위치 :: 순재(ansoonjae__)💗 ｜ 인장 :: 포포님(@ pooododo) 커미션",
+			"url": "https://t.co/gZi6STK5TA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/gZi6STK5TA",
+							"expanded_url": "http://www.twitch.tv/ansoonjae__",
+							"display_url": "twitch.tv/ansoonjae__",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 51,
+			"friends_count": 60,
+			"listed_count": 0,
+			"created_at": "Fri Aug 19 13:26:21 +0000 2016",
+			"favourites_count": 474,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 8519,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1438490071120826372/uM4smL_z_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1438490071120826372/uM4smL_z_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/766627363870257152/1637077624",
+			"profile_link_color": "B9A492",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 31878,
+		"favorite_count": 18791,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "새벽에 몰래웃다가 침흘릴뻔햇내 왇떠 빸!!!!!!!!!! https://t.co/bIKJr1b5az"
+	},
+	{
 		"created_at": "Thu Jan 06 23:19:10 +0000 2022",
 		"id": 1479231093610258400,
 		"id_str": "1479231093610258432",
