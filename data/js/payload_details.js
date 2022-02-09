f2638323972cfa5e0875e5ec11a52ed4d2336f82
ggongkui <32505785+ggongkui@users.noreply.github.com>
2022-02-09 09:15:31 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8856,
-	"created_at": "Mon Jan 31 00:15:29 +0000 2022",
+	"tweets": 8857,
+	"created_at": "Wed Feb 09 00:15:29 +0000 2022",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2022_02.js
@@ -0,0 +1,188 @@
+Grailbird.data.tweets_2022_02 = 
+[
+	{
+		"created_at": "Tue Feb 08 17:42:47 +0000 2022",
+		"id": 1478699708487200800,
+		"id_str": "1491105242251132929",
+		"full_text": "눈꺼풀 가장자리에 속눈썹이 나오는 두께감과 돌출되는 흐름을 만들면 디테일과 입체감이 생긴다 단 그림체에 따라 표현 차이가 있다 https://t.co/pCZCRkPGXt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			70
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1478699691454136300,
+					"id_str": "1478699691454136321",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/FIVliO6agAEKgtE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIVliO6agAEKgtE.jpg",
+					"url": "https://t.co/pCZCRkPGXt",
+					"display_url": "pic.twitter.com/pCZCRkPGXt",
+					"expanded_url": "https://twitter.com/taco1704/status/1478699708487200768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1380,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 939,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 532,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1478699691454136300,
+					"id_str": "1478699691454136321",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/FIVliO6agAEKgtE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIVliO6agAEKgtE.jpg",
+					"url": "https://t.co/pCZCRkPGXt",
+					"display_url": "pic.twitter.com/pCZCRkPGXt",
+					"expanded_url": "https://twitter.com/taco1704/status/1478699708487200768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1380,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 939,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 532,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 800159334105641000,
+			"id_str": "800159334105640960",
+			"name": "타코작가",
+			"screen_name": "taco1704",
+			"location": "대한민국",
+			"description": "프로픽 드로잉 강사 / 웹툰 작가 / 게임, 애니 컨셉 디자이너 / 이모티콘 작가 https://t.co/iDaJdVvrD5 드로잉 책 출간 “시크릿/포인트캐릭터드로잉”을 검색하세요~^^",
+			"url": "https://t.co/OYqSXtDlis",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OYqSXtDlis",
+							"expanded_url": "https://cafe.naver.com/propic7/2093",
+							"display_url": "cafe.naver.com/propic7/2093",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/iDaJdVvrD5",
+							"expanded_url": "http://linktr.ee/taco1704",
+							"display_url": "linktr.ee/taco1704",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 207024,
+			"friends_count": 495,
+			"listed_count": 1411,
+			"created_at": "Sun Nov 20 02:10:26 +0000 2016",
+			"favourites_count": 580,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 839,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/859083388506058753/fGH29dKQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/859083388506058753/fGH29dKQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/800159334105640960/1542282199",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5243,
+		"favorite_count": 17910,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "눈꺼풀 가장자리에 속눈썹이 나오는 두께감과 돌출되는 흐름을 만들면 디테일과 입체감이 생긴다 단 그림체에 따라 표현 차이가 있다 https://t.co/pCZCRkPGXt"
+	}
+]
