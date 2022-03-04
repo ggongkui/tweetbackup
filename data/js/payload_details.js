a6a49df1910fdb7e8ea21b39df85d9b21a492bce
ggongkui <32505785+ggongkui@users.noreply.github.com>
2022-03-04 09:15:32 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8864,
-	"created_at": "Tue Mar 01 00:15:29 +0000 2022",
+	"tweets": 8865,
+	"created_at": "Fri Mar 04 00:15:29 +0000 2022",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2022_03.js
@@ -0,0 +1,260 @@
+Grailbird.data.tweets_2022_03 = 
+[
+	{
+		"created_at": "Thu Mar 03 14:48:40 +0000 2022",
+		"id": 1415364072086794200,
+		"id_str": "1499396344779198464",
+		"full_text": "피부 채색할 때 베이스 컬러는 아주! 밝은 회색으로 깔아주면 살짝 투명하면서 혈색이 사는 피부를 칠하기가 좋아요\n1차 명암은 베이스 컬러에서 채도만 높여서 눈 주변/볼터치/입술/목 전체 깔고 2차 명암은 그림 톤에 맞춰서 채도/명도/색상을 조절해서 칠해주면 실패할 일 없는듯해요 https://t.co/Gmb3eXW4Gu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			155
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1415363411643302000,
+					"id_str": "1415363411643301890",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/E6RhhiMVgAINS0n.png",
+					"media_url_https": "https://pbs.twimg.com/media/E6RhhiMVgAINS0n.png",
+					"url": "https://t.co/Gmb3eXW4Gu",
+					"display_url": "pic.twitter.com/Gmb3eXW4Gu",
+					"expanded_url": "https://twitter.com/vnsl0809/status/1415364072086794246/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 661,
+							"h": 781,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 576,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 661,
+							"h": 781,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1415363411643302000,
+					"id_str": "1415363411643301890",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/E6RhhiMVgAINS0n.png",
+					"media_url_https": "https://pbs.twimg.com/media/E6RhhiMVgAINS0n.png",
+					"url": "https://t.co/Gmb3eXW4Gu",
+					"display_url": "pic.twitter.com/Gmb3eXW4Gu",
+					"expanded_url": "https://twitter.com/vnsl0809/status/1415364072086794246/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 661,
+							"h": 781,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 576,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 661,
+							"h": 781,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1415363411655884800,
+					"id_str": "1415363411655884800",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/E6RhhiPVgAAzHJZ.png",
+					"media_url_https": "https://pbs.twimg.com/media/E6RhhiPVgAAzHJZ.png",
+					"url": "https://t.co/Gmb3eXW4Gu",
+					"display_url": "pic.twitter.com/Gmb3eXW4Gu",
+					"expanded_url": "https://twitter.com/vnsl0809/status/1415364072086794246/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 690,
+							"h": 695,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 675,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 690,
+							"h": 695,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1415363411647504400,
+					"id_str": "1415363411647504389",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/E6RhhiNVoAUWgIJ.png",
+					"media_url_https": "https://pbs.twimg.com/media/E6RhhiNVoAUWgIJ.png",
+					"url": "https://t.co/Gmb3eXW4Gu",
+					"display_url": "pic.twitter.com/Gmb3eXW4Gu",
+					"expanded_url": "https://twitter.com/vnsl0809/status/1415364072086794246/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 418,
+							"h": 479,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 418,
+							"h": 479,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 418,
+							"h": 479,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2939054624,
+			"id_str": "2939054624",
+			"name": "반여름🐳😷",
+			"screen_name": "vnsl0809",
+			"location": "",
+			"description": "비팔 알림 OFF ㅣ 커미션 닫았습니다! ㅣ 22년 상반기 외주 일정 마감됐습니다! ✉️mail : vnsl0809@gmail.com ㅣ insta : summer__noxon__ ㅣpixiv : https://t.co/uDzDs51tmm",
+			"url": "https://t.co/BR7MCD66cI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BR7MCD66cI",
+							"expanded_url": "https://open.kakao.com/o/sSEh8ffb",
+							"display_url": "open.kakao.com/o/sSEh8ffb",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uDzDs51tmm",
+							"expanded_url": "http://pixiv.net/users/15139265",
+							"display_url": "pixiv.net/users/15139265",
+							"indices": [
+								110,
+								133
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2222,
+			"friends_count": 282,
+			"listed_count": 13,
+			"created_at": "Mon Dec 22 07:14:40 +0000 2014",
+			"favourites_count": 14922,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 39234,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1483302440342798340/gxUjkp1C_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1483302440342798340/gxUjkp1C_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2939054624/1602878102",
+			"profile_link_color": "7FDBB6",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6731,
+		"favorite_count": 6686,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "피부 채색할 때 베이스 컬러는 아주! 밝은 회색으로 깔아주면 살짝 투명하면서 혈색이 사는 피부를 칠하기가 좋아요\n1차 명암은 베이스 컬러에서 채도만 높여서 눈 주변/볼터치/입술/목 전체 깔고 2차 명암은 그림 톤에 맞춰서 채도/명도/색상을 조절해서 칠해주면 실패할 일 없는듯해요 https://t.co/Gmb3eXW4Gu"
+	}
+]
