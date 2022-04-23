02991ee4c83a704e373bb230f5c449fc9d82ce0b
ggongkui <32505785+ggongkui@users.noreply.github.com>
2022-04-23 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8885,
-	"created_at": "Sun Apr 17 00:15:29 +0000 2022",
+	"tweets": 8886,
+	"created_at": "Sat Apr 23 00:15:29 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,188 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Fri Apr 22 01:31:28 +0000 2022",
+		"id": 1516339204657197000,
+		"id_str": "1517315116756180992",
+		"full_text": "LD 낙서퀄 커미션\n4월 22일(금) 오후 10시 폼으로 선착 3명 받습니다!\nhttps://t.co/Mg5TBXKyE7 https://t.co/On2kvjmFW9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			67
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Mg5TBXKyE7",
+					"expanded_url": "https://posty.pe/q6l4q9",
+					"display_url": "posty.pe/q6l4q9",
+					"indices": [
+						44,
+						67
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1516339146486612000,
+					"id_str": "1516339146486611968",
+					"indices": [
+						68,
+						91
+					],
+					"media_url": "http://pbs.twimg.com/media/FQsea3lagAAEyf9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQsea3lagAAEyf9.jpg",
+					"url": "https://t.co/On2kvjmFW9",
+					"display_url": "pic.twitter.com/On2kvjmFW9",
+					"expanded_url": "https://twitter.com/comsunghe/status/1516339204657197063/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1062,
+							"h": 1068,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1062,
+							"h": 1068,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 676,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1516339146486612000,
+					"id_str": "1516339146486611968",
+					"indices": [
+						68,
+						91
+					],
+					"media_url": "http://pbs.twimg.com/media/FQsea3lagAAEyf9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQsea3lagAAEyf9.jpg",
+					"url": "https://t.co/On2kvjmFW9",
+					"display_url": "pic.twitter.com/On2kvjmFW9",
+					"expanded_url": "https://twitter.com/comsunghe/status/1516339204657197063/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1062,
+							"h": 1068,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1062,
+							"h": 1068,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 676,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1129045462298824700,
+			"id_str": "1129045462298824706",
+			"name": "숭션",
+			"screen_name": "comsunghe",
+			"location": "헤더는..만들어주셨음",
+			"description": "https://t.co/tnPNAfao76\n디엠X 메일로 문의\nsunghesung@naver.com",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/tnPNAfao76",
+							"expanded_url": "http://comsunghe.postype.com",
+							"display_url": "comsunghe.postype.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1631,
+			"friends_count": 3,
+			"listed_count": 20,
+			"created_at": "Thu May 16 15:26:29 +0000 2019",
+			"favourites_count": 3,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 59,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1351799296790650884/vYf2PjY__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1351799296790650884/vYf2PjY__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1129045462298824706/1589617537",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 654,
+		"favorite_count": 445,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "LD 낙서퀄 커미션\n4월 22일(금) 오후 10시 폼으로 선착 3명 받습니다!\nhttps://t.co/Mg5TBXKyE7 https://t.co/On2kvjmFW9"
+	},
+	{
 		"created_at": "Sat Apr 16 12:44:33 +0000 2022",
 		"id": 1514847328871805000,
 		"id_str": "1515310176634490885",
