58f14ae6b1bdb4f317023aace22defab4feaefcc
ggongkui <32505785+ggongkui@users.noreply.github.com>
2022-05-07 09:15:30 +0900

    Added 1 tweets

---
+++ data/js/tweets/2022_05.js
@@ -0,0 +1,178 @@
+Grailbird.data.tweets_2022_05 = 
+[
+	{
+		"created_at": "Fri May 06 09:41:08 +0000 2022",
+		"id": 1435429962425593900,
+		"id_str": "1522511772971724800",
+		"full_text": "兰波和小白 https://t.co/LLFWwJHtVE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			5
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1435429942678880300,
+					"id_str": "1435429942678880257",
+					"indices": [
+						6,
+						29
+					],
+					"media_url": "http://pbs.twimg.com/media/E-ur7XlVgAEYuTm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-ur7XlVgAEYuTm.jpg",
+					"url": "https://t.co/LLFWwJHtVE",
+					"display_url": "pic.twitter.com/LLFWwJHtVE",
+					"expanded_url": "https://twitter.com/BloodyBeni/status/1435429962425593861/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1032,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 343,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 605,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1435429942678880300,
+					"id_str": "1435429942678880257",
+					"indices": [
+						6,
+						29
+					],
+					"media_url": "http://pbs.twimg.com/media/E-ur7XlVgAEYuTm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-ur7XlVgAEYuTm.jpg",
+					"url": "https://t.co/LLFWwJHtVE",
+					"display_url": "pic.twitter.com/LLFWwJHtVE",
+					"expanded_url": "https://twitter.com/BloodyBeni/status/1435429962425593861/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1032,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 343,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 605,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2467151064,
+			"id_str": "2467151064",
+			"name": "紅花血月",
+			"screen_name": "BloodyBeni",
+			"location": "",
+			"description": "Beni(ɔ˘з˘)ɔ✦Commission Close🙏🏻不可转载私稿, 其他转载需标注绘者紅花血月🩸📮：honghuaxueyue2020@163.com",
+			"url": "https://t.co/PfwsLwCCso",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PfwsLwCCso",
+							"expanded_url": "http://www.pixiv.net/member.php?id=12613821",
+							"display_url": "pixiv.net/member.php?id=…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 181691,
+			"friends_count": 129,
+			"listed_count": 653,
+			"created_at": "Mon Apr 28 03:41:15 +0000 2014",
+			"favourites_count": 3403,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2064,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1475391625665060878/-5rPI3de_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1475391625665060878/-5rPI3de_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2467151064/1640595752",
+			"profile_link_color": "A00000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2409,
+		"favorite_count": 14415,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "zh",
+		"text": "兰波和小白 https://t.co/LLFWwJHtVE"
+	}
+]
