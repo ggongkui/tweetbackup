02c8e59e88e4b0036199a825402306f3b56ff0e6
ggongkui <32505785+ggongkui@users.noreply.github.com>
2022-08-21 09:15:30 +0900

    Added 1 tweets

--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,276 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sat Aug 20 07:44:02 +0000 2022",
+		"id": 1537370615757545500,
+		"id_str": "1560895418753372161",
+		"full_text": "요즘 갤러리 넘 칙칙해져서\n예전 그림이라도 섞어 올리기💃 https://t.co/BXCybsuAH0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			31
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1537370544500506600,
+					"id_str": "1537370544500506624",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/media/FVXWXWkaAAAaSGi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVXWXWkaAAAaSGi.jpg",
+					"url": "https://t.co/BXCybsuAH0",
+					"display_url": "pic.twitter.com/BXCybsuAH0",
+					"expanded_url": "https://twitter.com/mumu_086/status/1537370615757545474/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1525,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 894,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 506,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1537370544500506600,
+					"id_str": "1537370544500506624",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/media/FVXWXWkaAAAaSGi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVXWXWkaAAAaSGi.jpg",
+					"url": "https://t.co/BXCybsuAH0",
+					"display_url": "pic.twitter.com/BXCybsuAH0",
+					"expanded_url": "https://twitter.com/mumu_086/status/1537370615757545474/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1525,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 894,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 506,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1537370573432844300,
+					"id_str": "1537370573432844289",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/media/FVXWZCWacAET8hV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVXWZCWacAET8hV.jpg",
+					"url": "https://t.co/BXCybsuAH0",
+					"display_url": "pic.twitter.com/BXCybsuAH0",
+					"expanded_url": "https://twitter.com/mumu_086/status/1537370615757545474/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 407,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 718,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1226,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1537370588616196000,
+					"id_str": "1537370588616196098",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/media/FVXWZ66aAAIMoJ-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVXWZ66aAAIMoJ-.jpg",
+					"url": "https://t.co/BXCybsuAH0",
+					"display_url": "pic.twitter.com/BXCybsuAH0",
+					"expanded_url": "https://twitter.com/mumu_086/status/1537370615757545474/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1537370604583948300,
+					"id_str": "1537370604583948288",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/media/FVXWa2ZakAAlspl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVXWa2ZakAAlspl.jpg",
+					"url": "https://t.co/BXCybsuAH0",
+					"display_url": "pic.twitter.com/BXCybsuAH0",
+					"expanded_url": "https://twitter.com/mumu_086/status/1537370615757545474/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 436,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 769,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1312,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1001767493709455400,
+			"id_str": "1001767493709455360",
+			"name": "🤍𝑴𝑼𝑴𝑼🖤",
+			"screen_name": "mumu_086",
+			"location": "인장 챠무님/웹툰제의X ",
+			"description": "단발남캐 사냥꾼/개인외주/외주 mumu4952@naver.com",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 65682,
+			"friends_count": 611,
+			"listed_count": 262,
+			"created_at": "Wed May 30 10:09:17 +0000 2018",
+			"favourites_count": 9515,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28734,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1539578503334531073/_8kOoqyI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1539578503334531073/_8kOoqyI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1001767493709455360/1647867207",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 868,
+		"favorite_count": 4971,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "요즘 갤러리 넘 칙칙해져서\n예전 그림이라도 섞어 올리기💃 https://t.co/BXCybsuAH0"
+	},
+	{
 		"created_at": "Tue Aug 16 13:26:49 +0000 2022",
 		"id": 1559140373012021200,
 		"id_str": "1559532130371133440",
