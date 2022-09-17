34578b63ab45dc52d53b3172ee5d1ff129c6e529
ggongkui <32505785+ggongkui@users.noreply.github.com>
2022-09-17 09:15:30 +0900

    Added 1 tweets

--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,204 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Fri Sep 16 11:55:48 +0000 2022",
+		"id": 1570743249404563500,
+		"id_str": "1570743249404563456",
+		"full_text": "9/16 금요일\n아메리카노 2잔 + 물 많이\n도봉산&gt;집 산책 https://t.co/WRtATpIMdR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			36
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1570743243243147300,
+					"id_str": "1570743243243147264",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/FcxmqFbaUAAOYBV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcxmqFbaUAAOYBV.jpg",
+					"url": "https://t.co/WRtATpIMdR",
+					"display_url": "pic.twitter.com/WRtATpIMdR",
+					"expanded_url": "https://twitter.com/PF_Si2/status/1570743249404563456/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 849,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1526,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1570743243243147300,
+					"id_str": "1570743243243147264",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/FcxmqFbaUAAOYBV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcxmqFbaUAAOYBV.jpg",
+					"url": "https://t.co/WRtATpIMdR",
+					"display_url": "pic.twitter.com/WRtATpIMdR",
+					"expanded_url": "https://twitter.com/PF_Si2/status/1570743249404563456/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 849,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1526,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1570743247001227300,
+					"id_str": "1570743247001227269",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/FcxmqTbaEAUkGRw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcxmqTbaEAUkGRw.jpg",
+					"url": "https://t.co/WRtATpIMdR",
+					"display_url": "pic.twitter.com/WRtATpIMdR",
+					"expanded_url": "https://twitter.com/PF_Si2/status/1570743249404563456/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 634,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1159,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1159,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1570384750610878500,
+		"in_reply_to_status_id_str": "1570384750610878467",
+		"in_reply_to_user_id": 815064414,
+		"in_reply_to_user_id_str": "815064414",
+		"in_reply_to_screen_name": "PF_Si2",
+		"user": {
+			"id": 815064414,
+			"id_str": "815064414",
+			"name": "?¿",
+			"screen_name": "PF_Si2",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 2,
+			"friends_count": 6,
+			"listed_count": 0,
+			"created_at": "Mon Sep 10 11:53:42 +0000 2012",
+			"favourites_count": 113,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1642,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1214959584642654208/2Ag5dpXE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1214959584642654208/2Ag5dpXE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/815064414/1578503912",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "9/16 금요일\n아메리카노 2잔 + 물 많이\n도봉산&gt;집 산책 https://t.co/WRtATpIMdR"
+	},
+	{
 		"created_at": "Thu Sep 15 12:11:15 +0000 2022",
 		"id": 1570384750610878500,
 		"id_str": "1570384750610878467",
