7dad5956469ff886b3266a20314e18c591f11388
ggongkui <32505785+ggongkui@users.noreply.github.com>
2022-10-10 09:15:30 +0900

    Added 1 tweets

--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Sun Oct 09 03:58:19 +0000 2022",
+		"id": 1241375670292418600,
+		"id_str": "1578958010353082373",
+		"full_text": "미코테\n\n스칼라 수호자 외투(Lv.70, 스칼라 유적 )\n알라미고 바지(Lv.70, 센추리오 휘장 교환)\n프테로닥틸루스 신발(제작, 장게 구매) https://t.co/spN2CC2v62",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			80
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1241375649857564700,
+					"id_str": "1241375649857564673",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/ETpAk5KUcAEMnUV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETpAk5KUcAEMnUV.jpg",
+					"url": "https://t.co/spN2CC2v62",
+					"display_url": "pic.twitter.com/spN2CC2v62",
+					"expanded_url": "https://twitter.com/ffxivlook_w/status/1241375670292418561/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 360,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 542,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 542,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1241375649857564700,
+					"id_str": "1241375649857564673",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/ETpAk5KUcAEMnUV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETpAk5KUcAEMnUV.jpg",
+					"url": "https://t.co/spN2CC2v62",
+					"display_url": "pic.twitter.com/spN2CC2v62",
+					"expanded_url": "https://twitter.com/ffxivlook_w/status/1241375670292418561/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 360,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 542,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 542,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1241375659990999000,
+					"id_str": "1241375659990999042",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/ETpAle6UYAIR63Z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETpAle6UYAIR63Z.jpg",
+					"url": "https://t.co/spN2CC2v62",
+					"display_url": "pic.twitter.com/spN2CC2v62",
+					"expanded_url": "https://twitter.com/ffxivlook_w/status/1241375670292418561/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 360,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 542,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 542,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1239029654222188500,
+			"id_str": "1239029654222188544",
+			"name": "파판14 여캐 옷장",
+			"screen_name": "ffxivlook_w",
+			"location": "",
+			"description": "ffxiv 여캐 룩템을 정리하는 계정입니다! / only 여캐👍🏻 / 한섭 유저 / 지포스 필터O 모드X",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 665,
+			"friends_count": 0,
+			"listed_count": 6,
+			"created_at": "Sun Mar 15 03:24:59 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 49,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1239070891729813506/J11f9SaV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1239070891729813506/J11f9SaV_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 20,
+		"favorite_count": 19,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "미코테\n\n스칼라 수호자 외투(Lv.70, 스칼라 유적 )\n알라미고 바지(Lv.70, 센추리오 휘장 교환)\n프테로닥틸루스 신발(제작, 장게 구매) https://t.co/spN2CC2v62"
+	},
+	{
 		"created_at": "Wed Oct 05 23:42:26 +0000 2022",
 		"id": 1577591222155501600,
 		"id_str": "1577806448280862721",
