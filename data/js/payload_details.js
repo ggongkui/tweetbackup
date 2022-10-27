bf7ff2f45c4875f71c041081fd7f9163625d3ea1
ggongkui <32505785+ggongkui@users.noreply.github.com>
2022-10-27 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8949,
-	"created_at": "Mon Oct 24 00:15:29 +0000 2022",
+	"tweets": 8950,
+	"created_at": "Thu Oct 27 00:15:29 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Wed Oct 26 14:53:05 +0000 2022",
+		"id": 1584011361906208800,
+		"id_str": "1585283379825549312",
+		"full_text": "https://t.co/rJdLOCrSvx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1584010939304648700,
+					"id_str": "1584010939304648709",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FfuJjo2WIAUL0zY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FfuJjo2WIAUL0zY.jpg",
+					"url": "https://t.co/rJdLOCrSvx",
+					"display_url": "pic.twitter.com/rJdLOCrSvx",
+					"expanded_url": "https://twitter.com/Bamme_o3o/status/1584011361906208768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 499,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1503,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 881,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1584010939304648700,
+					"id_str": "1584010939304648709",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FfuJjo2WIAUL0zY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FfuJjo2WIAUL0zY.jpg",
+					"url": "https://t.co/rJdLOCrSvx",
+					"display_url": "pic.twitter.com/rJdLOCrSvx",
+					"expanded_url": "https://twitter.com/Bamme_o3o/status/1584011361906208768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 499,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1503,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 881,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 866863903979872300,
+			"id_str": "866863903979872258",
+			"name": "밤메",
+			"screen_name": "Bamme_o3o",
+			"location": "팬딩",
+			"description": "🔕",
+			"url": "https://t.co/Nyltpu0Ctl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Nyltpu0Ctl",
+							"expanded_url": "https://fanding.kr/user/AivuY3TqSta6/membership/3500",
+							"display_url": "fanding.kr/user/AivuY3TqS…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 29483,
+			"friends_count": 197,
+			"listed_count": 127,
+			"created_at": "Tue May 23 03:50:35 +0000 2017",
+			"favourites_count": 10716,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11468,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1581248403987734528/7J2_kybm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1581248403987734528/7J2_kybm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/866863903979872258/1665704795",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1036,
+		"favorite_count": 6281,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "zxx",
+		"text": "https://t.co/rJdLOCrSvx"
+	},
+	{
 		"created_at": "Sun Oct 23 13:15:00 +0000 2022",
 		"id": 1578622828651327500,
 		"id_str": "1584171531550461953",
