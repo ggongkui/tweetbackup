42464323ef19dc8c8b70e4c904a6abd92a2f558a
ggongkui <32505785+ggongkui@users.noreply.github.com>
2022-11-03 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8951,
-	"created_at": "Tue Nov 01 00:15:30 +0000 2022",
+	"tweets": 8952,
+	"created_at": "Thu Nov 03 00:15:29 +0000 2022",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2022_11.js
@@ -0,0 +1,260 @@
+Grailbird.data.tweets_2022_11 = 
+[
+	{
+		"created_at": "Wed Nov 02 06:44:08 +0000 2022",
+		"id": 1587284009654005800,
+		"id_str": "1587697047666556929",
+		"full_text": "앉아있거나 누워있는 낮은자세를 모아놓은 자료. 서있는 크로키 자료는 많지만 이런 다채로운 동세를 취하는 낮은자세는 구하기가 힘듭니다. 진짜 썸네일 사진 잘 모아놨네요 ㅎㅎㅎ \n출처:  https://t.co/9V3hiffptT https://t.co/XrSIxBp8xX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			126
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9V3hiffptT",
+					"expanded_url": "https://www.artstation.com/blogs/satinezillah/jygd/nwe-reference-pack-sales",
+					"display_url": "artstation.com/blogs/satinezi…",
+					"indices": [
+						103,
+						126
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1587283653855309800,
+					"id_str": "1587283653855309825",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/FgcqEsnUcAEvKLG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FgcqEsnUcAEvKLG.jpg",
+					"url": "https://t.co/XrSIxBp8xX",
+					"display_url": "pic.twitter.com/XrSIxBp8xX",
+					"expanded_url": "https://twitter.com/shaonav/status/1587284009654005760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1301,
+							"h": 733,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 676,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1587283653855309800,
+					"id_str": "1587283653855309825",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/FgcqEsnUcAEvKLG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FgcqEsnUcAEvKLG.jpg",
+					"url": "https://t.co/XrSIxBp8xX",
+					"display_url": "pic.twitter.com/XrSIxBp8xX",
+					"expanded_url": "https://twitter.com/shaonav/status/1587284009654005760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1301,
+							"h": 733,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 676,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1587283662571073500,
+					"id_str": "1587283662571073537",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/FgcqFNFUcAEv6fL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FgcqFNFUcAEv6fL.jpg",
+					"url": "https://t.co/XrSIxBp8xX",
+					"display_url": "pic.twitter.com/XrSIxBp8xX",
+					"expanded_url": "https://twitter.com/shaonav/status/1587284009654005760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1305,
+							"h": 733,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 674,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1587283668376031200,
+					"id_str": "1587283668376031232",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/FgcqFitVEAAT9sM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FgcqFitVEAAT9sM.jpg",
+					"url": "https://t.co/XrSIxBp8xX",
+					"display_url": "pic.twitter.com/XrSIxBp8xX",
+					"expanded_url": "https://twitter.com/shaonav/status/1587284009654005760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 384,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 677,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1297,
+							"h": 732,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 468432183,
+			"id_str": "468432183",
+			"name": "인슈_Art ClasShoo",
+			"screen_name": "shaonav",
+			"location": "경기도 성남시 판교",
+			"description": "맞팔 환영합니다^^ '이 그림은 내가집도한다' 작가 & 게임 원화가 인슈입니다. '클라슈'유투브를 운영하고 있어요 많이들 구독해주세요",
+			"url": "https://t.co/WRKa5l0jOF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WRKa5l0jOF",
+							"expanded_url": "https://www.youtube.com/2wells",
+							"display_url": "youtube.com/2wells",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12874,
+			"friends_count": 1226,
+			"listed_count": 84,
+			"created_at": "Thu Jan 19 14:49:14 +0000 2012",
+			"favourites_count": 4311,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1907,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/855437200711852033/x_AvrHrg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/855437200711852033/x_AvrHrg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/468432183/1572455473",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3811,
+		"favorite_count": 4399,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "앉아있거나 누워있는 낮은자세를 모아놓은 자료. 서있는 크로키 자료는 많지만 이런 다채로운 동세를 취하는 낮은자세는 구하기가 힘듭니다. 진짜 썸네일 사진 잘 모아놨네요 ㅎㅎㅎ \n출처:  https://t.co/9V3hiffptT https://t.co/XrSIxBp8xX"
+	}
+]
