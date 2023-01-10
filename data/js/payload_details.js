c32bfae6ee97a7c8deb9c321e0c2c5e293d989c4
ggongkui <32505785+ggongkui@users.noreply.github.com>
2023-01-10 09:15:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8978,
-	"created_at": "Thu Jan 05 00:15:30 +0000 2023",
+	"tweets": 8979,
+	"created_at": "Tue Jan 10 00:15:29 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Mon Jan 09 15:54:08 +0000 2023",
+		"id": 1612366449347264500,
+		"id_str": "1612477834001514496",
+		"full_text": ".51~53✍️ https://t.co/glklzmU5tf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			8
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1612366134795464700,
+					"id_str": "1612366134795464706",
+					"indices": [
+						9,
+						32
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1612366134795464706/pu/img/GTsLVauuJEUYms-D.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1612366134795464706/pu/img/GTsLVauuJEUYms-D.jpg",
+					"url": "https://t.co/glklzmU5tf",
+					"display_url": "pic.twitter.com/glklzmU5tf",
+					"expanded_url": "https://twitter.com/shi_b_a/status/1612366449347264512/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1612366134795464700,
+					"id_str": "1612366134795464706",
+					"indices": [
+						9,
+						32
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1612366134795464706/pu/img/GTsLVauuJEUYms-D.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1612366134795464706/pu/img/GTsLVauuJEUYms-D.jpg",
+					"url": "https://t.co/glklzmU5tf",
+					"display_url": "pic.twitter.com/glklzmU5tf",
+					"expanded_url": "https://twitter.com/shi_b_a/status/1612366449347264512/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 31733,
+						"variants": [
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1612366134795464706/pu/vid/720x720/fnSPjy8h7gJWAuSR.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1612366134795464706/pu/vid/540x540/AA4KppqHIWWg_Va4.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1612366134795464706/pu/pl/LK9YhukTpUW0SJQi.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1612366134795464706/pu/vid/320x320/4zEnGEBvgxuQUshe.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4277376380,
+			"id_str": "4277376380",
+			"name": "芝",
+			"screen_name": "shi_b_a",
+			"location": "現在企業案件のみ相談可能です",
+			"description": "絵描き 画像無断利用トレスAI学習→NGです Mail| t.shiba1710@gmail.com pixiv|https://t.co/mAM7kCo00l　skeb|https://t.co/d0s5qsi0Q7 FND| https://t.co/3fOd4947F8 練習垢@shi_b_a_sub",
+			"url": "https://t.co/C7EN26dH2w",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/C7EN26dH2w",
+							"expanded_url": "http://www.shiba-illustration.com",
+							"display_url": "shiba-illustration.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/mAM7kCo00l",
+							"expanded_url": "https://www.pixiv.net/users/11974382",
+							"display_url": "pixiv.net/users/11974382",
+							"indices": [
+								57,
+								80
+							]
+						},
+						{
+							"url": "https://t.co/d0s5qsi0Q7",
+							"expanded_url": "http://skeb.jp/@shi_b_a",
+							"display_url": "skeb.jp/@shi_b_a",
+							"indices": [
+								86,
+								109
+							]
+						},
+						{
+							"url": "https://t.co/3fOd4947F8",
+							"expanded_url": "http://foundation.app/@shi_b_a",
+							"display_url": "foundation.app/@shi_b_a",
+							"indices": [
+								115,
+								138
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 102144,
+			"friends_count": 623,
+			"listed_count": 850,
+			"created_at": "Wed Nov 18 09:37:28 +0000 2015",
+			"favourites_count": 10146,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3627,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1006139566703632384/tQMcM0L5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1006139566703632384/tQMcM0L5_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4277376380/1672502199",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5947,
+		"favorite_count": 34043,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": ".51~53✍️ https://t.co/glklzmU5tf"
+	},
+	{
 		"created_at": "Wed Jan 04 22:49:18 +0000 2023",
 		"id": 1610751169600839700,
 		"id_str": "1610770375146876930",
